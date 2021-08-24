43d181ec6274f053ac70ee6ccb0279f30890d469
syahmighafar <81664143+syahmighafar@users.noreply.github.com>
2021-08-24 10:44:33 +0800

    Create panaga.js

---
+++ panaga.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+	$(document).ready(function(){
+	
+		$("#uploadarea").hide();
+		$("#paymentnoticearea").hide();
+		$("#uploadbruhealthproof").attr("required", "false");
+
+     		$('#Currently-in-Quarantine').change(function(){
+	     
+     			if($(this).val()=="Yes") {
+				$("#uploadarea").show();
+				$("#uploadbruhealthproof").attr("required", "true");
+     			}
+			
+     			if($(this).val()=="No") {
+				$("#uploadarea").hide();
+	     			$("#uploadbruhealthproof").attr("required", "false");
+			}
+		});
+		
+		$('#Payment-Method').change(function(){
+	     
+     			if($(this).val()=="Bank Transfer") {
+				$("#paymentnoticearea").show();
+     			}
+			
+     			if($(this).val()=="Cash on Delivery") {
+				$("#paymentnoticearea").hide();
+			}
+		});
+	});	
+});

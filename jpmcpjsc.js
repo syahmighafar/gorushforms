e74a5be9f721b430a20f56b5d7b392f75433aa35
syahmighafar <81664143+syahmighafar@users.noreply.github.com>
2021-08-24 10:29:45 +0800

    Create jpmcpjsc.js

---
+++ jpmcpjsc.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+	$(document).ready(function(){
+	
+		$("#uploadarea").hide();
+		$("#paymentnoticearea").hide();
+		$("#uploadbruhealthproof").attr("required", "false");
+
+     		$('#Currently-in-Quarantine').change(function(){
+	     
+     			if($(this).val()=="Yes") {
+				$("#uploadarea").show();
+				$("#uploadbruhealthproof").attr("required", "true");
+     			}
+			
+     			if($(this).val()=="No") {
+				$("#uploadarea").hide();
+	     			$("#uploadbruhealthproof").attr("required", "false");
+			}
+		});
+		
+		$('#Payment-Method').change(function(){
+	     
+     			if($(this).val()=="Bank Transfer") {
+				$("#paymentnoticearea").show();
+     			}
+			
+     			if($(this).val()=="Cash on Delivery") {
+				$("#paymentnoticearea").hide();
+			}
+		});
+    
+    $('#JPMC-or-PJSC').change(function(){
+	     
+      			if($(this).val()=="JPMC") {
+        			$("#Patient-Number-2").attr("minlength", "8");
+				$("#Patient-Number-2").attr("maxlength", "8");
+        			$("#Patient-Number-2").attr("size", "8");
+      			}
+      		
+			if($(this).val()=="PJSC") {
+	      			$("#Patient-Number-2").attr("minlength", "6");
+				$("#Patient-Number-2").attr("maxlength", "6");
+        			$("#Patient-Number-2").attr("size", "6");
+      			}
+    		});
+	});	
+});

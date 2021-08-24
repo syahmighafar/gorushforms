915213769f1d591f5aea316278b6373b0abacfb0
syahmighafar <81664143+syahmighafar@users.noreply.github.com>
2021-08-24 10:36:06 +0800

    Create ripastemp.js

---
+++ ripastemp.js
@@ -0,0 +1,54 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+	$(document).ready(function(){
+	
+		$("#uploadarea").hide();
+		$("#normalarea").show();
+		$("#botharea").hide();
+		$("#easycollectarea").hide();
+		$("#autorefillarea").hide();
+		$("#confirmationarea").show();
+		$("#paymentnoticearea").hide();
+		
+		document.getElementById("Full-Name-2").required = true;
+		document.getElementById("Bru-HIMS-Number").required = true;
+		document.getElementById("Date-of-Birth").required = true;
+		document.getElementById("IC-Number").required = true;
+		document.getElementById("Contact-Number").required = true;
+		document.getElementById("Delivery-Address-2").required = true;
+		document.getElementById("Payment-Method").required = true;
+		document.getElementById("Remarks").required = true;
+	
+		document.getElementById("uploadbruhealthproof").required = false;
+		
+		document.getElementById("Type-Of-Mandatory").required = true;
+
+     		$('#Currently-in-Quarantine').change(function(){
+	     
+     			if($(this).val()=="Yes") {
+
+				$("#uploadarea").show();
+	     			document.getElementById("uploadbruhealthproof").required = true;
+     			}
+		
+     			if($(this).val()=="No") {
+	     
+				$("#uploadarea").hide();
+	     			document.getElementById("uploadbruhealthproof").required = false;   
+     			}
+		});
+    
+    $('#Payment-Method').change(function(){
+	     
+     					if($(this).val()=="Bank Transfer") {
+						$("#paymentnoticearea").show();
+     					}
+			
+     					if($(this).val()=="Cash on Delivery") {
+						$("#paymentnoticearea").hide();
+					}
+			});
+
+	});
+		
+});

e8853b975949ba8253c78318ad63331721939084
syahmighafar <81664143+syahmighafar@users.noreply.github.com>
2021-08-28 11:53:33 +0800

    Update ripas.js

--- ripas.js
+++ ripas.js
@@ -101,12 +101,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
 				$("#easycollectarea").hide();
 				$("#autorefillarea").hide();
 				
-				document.getElementById("Full-Name-2").required = false;
+				document.getElementById("Full-Name").required = false;
 				document.getElementById("Bru-HIMS-Number").required = false;
 				document.getElementById("Date-of-Birth").required = false;
 				document.getElementById("IC-Number").required = false;
 				document.getElementById("Contact-Number").required = false;
-				document.getElementById("Delivery-Address-2").required = false;
+				document.getElementById("Delivery-Address").required = false;
 
      			}
 	     
@@ -117,22 +117,22 @@ document.addEventListener("DOMContentLoaded", function(event) {
 				$("#easycollectarea").hide();
 				$("#autorefillarea").hide();
 				
-				document.getElementById("Full-Name-2").required = true;
+				document.getElementById("Full-Name").required = true;
 				document.getElementById("Bru-HIMS-Number").required = true;
 				document.getElementById("Date-of-Birth").required = true;
 				document.getElementById("IC-Number").required = true;
 				document.getElementById("Contact-Number").required = true;
-				document.getElementById("Delivery-Address-2").required = true;
+				document.getElementById("Delivery-Address").required = true;
 				
 				document.getElementById("submitnewregistrationbutton").addEventListener("click", function() {
-  					if (($("#Full-Name-2").val().length == 0 ) || ($("#Bru-HIMS-Number").val().length == 0 ) ||
+  					if (($("#Full-Name").val().length == 0 ) || ($("#Bru-HIMS-Number").val().length == 0 ) ||
 				    ($("#Date-of-Birth").val().length == 0 ) || ($("#IC-Number").val().length == 0 ) ||
-				    ($("#Contact-Number").val().length == 0) || ($("#Delivery-Address-2").val().length == 0)) {
+				    ($("#Contact-Number").val().length == 0) || ($("#Delivery-Address").val().length == 0)) {
 					$("#otherarea").hide();
 				}
 				
-				if (($("#Full-Name-2").val().length != 0) && ($("#Bru-HIMS-Number").val().length != 0) && ($("#Date-of-Birth").val().length != 0) &&
-				    ($("#IC-Number").val().length != 0) && ($("#Contact-Number").val().length != 0) && ($("#Delivery-Address-2").val().length != 0)) {
+				if (($("#Full-Name").val().length != 0) && ($("#Bru-HIMS-Number").val().length != 0) && ($("#Date-of-Birth").val().length != 0) &&
+				    ($("#IC-Number").val().length != 0) && ($("#Contact-Number").val().length != 0) && ($("#Delivery-Address").val().length != 0)) {
 					$("#submitnewregistrationbutton").hide();
 					$("#otherarea").fadeIn();
 				}
@@ -150,12 +150,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
 				$("#easycollectarea").fadeIn();
 				$("#autorefillarea").hide();
 				
-				document.getElementById("Full-Name-2").required = false;
+				document.getElementById("Full-Name").required = false;
 				document.getElementById("Bru-HIMS-Number").required = false;
 				document.getElementById("Date-of-Birth").required = false;
 				document.getElementById("IC-Number").required = false;
 				document.getElementById("Contact-Number").required = false;
-				document.getElementById("Delivery-Address-2").required = false;
+				document.getElementById("Delivery-Address").required = false;
 						      
 						      
 				
@@ -179,12 +179,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
 				$("#easycollectarea").hide();
 				$("#autorefillarea").fadeIn();
 				
-				document.getElementById("Full-Name-2").required = false;
+				document.getElementById("Full-Name").required = false;
 				document.getElementById("Bru-HIMS-Number").required = false;
 				document.getElementById("Date-of-Birth").required = false;
 				document.getElementById("IC-Number").required = false;
 				document.getElementById("Contact-Number").required = false;
-				document.getElementById("Delivery-Address-2").required = false;
+				document.getElementById("Delivery-Address").required = false;
       
 						      
 				

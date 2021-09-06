ccfb646ea418d97b71f9b14e814b9edadd080ac6
syahmighafar <81664143+syahmighafar@users.noreply.github.com>
2021-09-06 17:03:17 +0800

    Update jpmcpjscv2.js

--- jpmcpjscv2.js
+++ jpmcpjscv2.js
@@ -9,44 +9,27 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		$("#otherarea").hide();
 		$("#paymentnoticearea").hide();
 		$("#confirmationarea").hide();
+		$("#addressarea").hide();
+		$("#selfcollectdatearea").hide();
 
      		$('#Currently-in-Quarantine').change(function(){
 			
 			if($(this).val().length == 0) {
 				$("#uploadarea").hide();
-				$("#deliverytypearea").hide();
+				$("#jpmcpjscarea").hide();
 				
      			}
 	     
      			if($(this).val()=="Yes") {
 				$("#uploadarea").fadeIn();
-				$("#deliverytypearea").fadeIn();
+				$("#jpmcpjscarea").fadeIn();
 				
      			}
 		
      			if($(this).val()=="No") {
 				$("#uploadarea").hide();
-				$("#deliverytypearea").fadeIn();
-     			}
-		});
-		
-		$('#Type-Of-Delivery').change(function(){
-			
-			if ( $(this).val().length == 0 ) {
-				$("#expressnoticearea").hide();
-				$("#jpmcpjscarea").hide();
-				
-     			}
-			
-			if ( ( $(this).val().includes("Self Collect") ) || ( $(this).val().includes("Standard") ) ) {
-				$("#expressnoticearea").hide();
 				$("#jpmcpjscarea").fadeIn();
-			}
-
-			if ( $(this).val().includes("Express") ) {
-				$("#expressnoticearea").fadeIn();
-				$("#jpmcpjscarea").fadeIn();
-			}
+     			}
 		});
 		
 		$('#JPMC-or-PJSC').change(function(){
@@ -55,35 +38,99 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	      			$("#Patient-Number").attr("minlength", "0");
 				$("#Patient-Number").attr("maxlength", "0");
         			$("#Patient-Number").attr("size", "0");
-				$("#normalarea").hide();
+				$("#deliverytypearea").hide();
       			}
 	     
       			if($(this).val()=="JPMC") {
         			$("#Patient-Number").attr("minlength", "8");
 				$("#Patient-Number").attr("maxlength", "8");
         			$("#Patient-Number").attr("size", "8");
-				$("#normalarea").fadeIn();
+				$("#deliverytypearea").fadeIn();
       			}
       		
 			if($(this).val()=="PJSC") {
 	      			$("#Patient-Number").attr("minlength", "6");
 				$("#Patient-Number").attr("maxlength", "6");
         			$("#Patient-Number").attr("size", "6");
-				$("#normalarea").fadeIn();
+				$("#deliverytypearea").fadeIn();
       			}
     		});
 		
-		document.getElementById("submitregistrationbutton").addEventListener("click", function() {
-  			if (($("#Full-Name").val().length == 0 ) || ($("#Patient-Number").val().length == 0 ) ||  ($("#Contact-Number").val().length == 0) ||
-			($("#Delivery-Address").val().length == 0) || ($("#Postal-Code").val().length == 0) ) {
-				$("#otherarea").hide();
-				alert("Please do not leave the required fields empty!");
-			}
+		$('#Type-Of-Delivery').change(function(){
+			
+			if ( $(this).val().length == 0 ) {
+				$("#expressnoticearea").hide();
+				$("#normalarea").hide();
+				$("#addressarea").hide();
+				$("#selfcollectdatearea").hide();
 				
-			if (($("#Full-Name").val().length != 0) && ($("#Patient-Number").val().length != 0) && ($("#Contact-Number").val().length != 0) &&
-			($("#Delivery-Address").val().length != 0) && ($("#Postal-Code").val().length != 0) ) {
-				$("#submitregistrationbutton").hide();
-				$("#otherarea").fadeIn();
+     			}
+			
+			if ( ($(this).val().includes("Self Collect")) ) {
+				$("#expressnoticearea").hide();
+				$("#normalarea").fadeIn();
+				$("#addressarea").hide();
+				$("#selfcollectdatearea").fadeIn();
+				
+				document.getElementById("submitregistrationbutton").addEventListener("click", function() {
+  					if ( ($("#Full-Name").val().length == 0 ) || ($("#Patient-Number").val().length == 0 ) || ($("#Contact-Number").val().length == 0) ||
+					($("#Selfcollect-Date").val().length == 0) ) {
+						$("#otherarea").hide();
+						alert("Please do not leave the required fields empty!");
+					}
+				
+					if ( ($("#Full-Name").val().length != 0) && ($("#Patient-Number").val().length != 0) && ($("#Contact-Number").val().length != 0) &&
+					($("#Selfcollect-Date").val().length != 0) ) {
+						$("#submitregistrationbutton").hide();
+						$("#otherarea").fadeIn();
+					}
+				});
+			}
+			
+			if ( ($(this).val().includes("Standard")) ) {
+				$("#expressnoticearea").hide();
+				$("#normalarea").fadeIn();
+				$("#addressarea").fadeIn();
+				$("#selfcollectdatearea").hide();
+				
+				document.getElementById("submitregistrationbutton").addEventListener("click", function() {
+  					if ( ($("#Full-Name").val().length == 0 ) || ($("#Patient-Number").val().length == 0 ) || ($("#Contact-Number").val().length == 0) ||
+					($("#District").val().length == 0) || ($("#Kampung").val().length == 0) || ($("#Jalan-Location").val().length == 0) ||
+					($("#Simpang-Number").val().length == 0) || ($("#House-Unit-Number").val().length == 0) ) {
+						$("#otherarea").hide();
+						alert("Please do not leave the required fields empty!");
+					}
+				
+					if ( ($("#Full-Name").val().length != 0) && ($("#Patient-Number").val().length != 0) && ($("#Contact-Number").val().length != 0) &&
+					($("#District").val().length != 0) && ($("#Kampung").val().length != 0) && ($("#Jalan-Location").val().length != 0) &&
+					($("#Simpang-Number").val().length != 0) && ($("#House-Unit-Number").val().length != 0) ) {
+						$("#submitregistrationbutton").hide();
+						$("#otherarea").fadeIn();
+					}
+				});
+			}
+
+			if ( ($(this).val().includes("Express")) ) {
+				$("#expressnoticearea").fadeIn();
+				$("#normalarea").fadeIn();
+				$("#addressarea").fadeIn();
+				$("#selfcollectdatearea").hide();
+				
+				document.getElementById("submitregistrationbutton").addEventListener("click", function() {
+  					if ( ($("#Full-Name").val().length == 0 ) || ($("#Patient-Number").val().length == 0 ) || ($("#Contact-Number").val().length == 0) ||
+					($("#District").val().length == 0) || ($("#Kampung").val().length == 0) || ($("#Jalan-Location").val().length == 0) ||
+					($("#Simpang-Number").val().length == 0) || ($("#House-Unit-Number").val().length == 0) ) {
+						$("#otherarea").hide();
+						alert("Please do not leave the required fields empty!");
+					}
+				
+					if ( ($("#Full-Name").val().length != 0) && ($("#Patient-Number").val().length != 0) && ($("#Contact-Number").val().length != 0) &&
+					($("#District").val().length != 0) && ($("#Kampung").val().length != 0) && ($("#Jalan-Location").val().length != 0) &&
+					($("#Simpang-Number").val().length != 0) && ($("#House-Unit-Number").val().length != 0) ) {
+						$("#submitregistrationbutton").hide();
+						$("#otherarea").fadeIn();
+					}
+				});
 			}
 		});
 		

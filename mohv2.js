b8d7ba0d34790d990ba5730425fc1a17e2dd42a0
syahmighafar <81664143+syahmighafar@users.noreply.github.com>
2021-09-08 16:35:06 +0800

    Update mohv2.js

--- mohv2.js
+++ mohv2.js
@@ -43,16 +43,14 @@ document.addEventListener("DOMContentLoaded", function(event) {
 				$("#selfcollectdatearea").fadeIn();
 				
 				document.getElementById("submitregistrationbutton").addEventListener("click", function() {
-  					if ( ($("#Full-Name").val().length == 0 ) || ($("#Bru-HIMS-Number").val().length == 0 ) || ($("#Contact-Number").val().length == 0) ||
-					($("#Selfcollect-Date").val().length == 0) ) {
-						$("#otherarea").hide();
+  					if ( ($("#Full-Name").val().length == 0 ) || ($("#Bru-HIMS-Number").val().length == 0 ) || ($("#Selfcollect-Date").val().length == 0) ) {
+						$("#bruphonechoicearea").hide();
 						alert("Please do not leave the required fields empty!");
 					}
 				
-					if ( ($("#Full-Name").val().length != 0) && ($("#Bru-HIMS-Number").val().length != 0) && ($("#Contact-Number").val().length != 0) &&
-					($("#Selfcollect-Date").val().length != 0) ) {
+					if ( ($("#Full-Name").val().length != 0) && ($("#Bru-HIMS-Number").val().length != 0) && ($("#Selfcollect-Date").val().length != 0) ) {
 						$("#submitregistrationbutton").hide();
-						$("#otherarea").fadeIn();
+						$("#bruphonechoicearea").fadeIn();
 					}
 				});
      			}
@@ -64,18 +62,18 @@ document.addEventListener("DOMContentLoaded", function(event) {
 				$("#selfcollectdatearea").hide();
 				
 				document.getElementById("submitregistrationbutton").addEventListener("click", function() {
-  					if ( ($("#Full-Name").val().length == 0 ) || ($("#Bru-HIMS-Number").val().length == 0 ) || ($("#Contact-Number").val().length == 0) ||
-					($("#District").val().length == 0) || ($("#Kampung").val().length == 0) || ($("#Jalan-Location").val().length == 0) ||
+  					if ( ($("#Full-Name").val().length == 0 ) || ($("#Bru-HIMS-Number").val().length == 0 ) || ($("#District").val().length == 0) ||
+					($("#Kampung").val().length == 0) || ($("#Jalan-Location").val().length == 0) ||
 					($("#Simpang-Number").val().length == 0) || ($("#House-Unit-Number").val().length == 0) ) {
-						$("#otherarea").hide();
+						$("#bruphonechoicearea").hide();
 						alert("Please do not leave the required fields empty!");
 					}
 				
-					if ( ($("#Full-Name").val().length != 0) && ($("#Bru-HIMS-Number").val().length != 0) && ($("#Contact-Number").val().length != 0) &&
-					($("#District").val().length != 0) && ($("#Kampung").val().length != 0) && ($("#Jalan-Location").val().length != 0) &&
+					if ( ($("#Full-Name").val().length != 0) && ($("#Bru-HIMS-Number").val().length != 0) && ($("#District").val().length != 0) &&
+					($("#Kampung").val().length != 0) && ($("#Jalan-Location").val().length != 0) &&
 					($("#Simpang-Number").val().length != 0) && ($("#House-Unit-Number").val().length != 0) ) {
 						$("#submitregistrationbutton").hide();
-						$("#otherarea").fadeIn();
+						$("#bruphonechoicearea").fadeIn();
 					}
 				});
      			}
@@ -87,17 +85,64 @@ document.addEventListener("DOMContentLoaded", function(event) {
 				$("#selfcollectdatearea").hide();
 				
 				document.getElementById("submitregistrationbutton").addEventListener("click", function() {
-  					if ( ($("#Full-Name").val().length == 0 ) || ($("#Bru-HIMS-Number").val().length == 0 ) || ($("#Contact-Number").val().length == 0) ||
-					($("#District").val().length == 0) || ($("#Kampung").val().length == 0) || ($("#Jalan-Location").val().length == 0) ||
+  					if ( ($("#Full-Name").val().length == 0 ) || ($("#Bru-HIMS-Number").val().length == 0 ) || ($("#District").val().length == 0) ||
+					($("#Kampung").val().length == 0) || ($("#Jalan-Location").val().length == 0) ||
 					($("#Simpang-Number").val().length == 0) || ($("#House-Unit-Number").val().length == 0) ) {
+						$("#bruphonechoicearea").hide();
+						alert("Please do not leave the required fields empty!");
+					}
+				
+					if ( ($("#Full-Name").val().length != 0) && ($("#Bru-HIMS-Number").val().length != 0) && ($("#District").val().length != 0) &&
+					($("#Kampung").val().length != 0) && ($("#Jalan-Location").val().length != 0) &&
+					($("#Simpang-Number").val().length != 0) && ($("#House-Unit-Number").val().length != 0) ) {
+						$("#submitregistrationbutton").hide();
+						$("#bruphonechoicearea").fadeIn();
+					}
+				});
+			}
+		});
+		
+		$('#Using-Brunei-Phone-Number').change(function(){
+			if($(this).val().length == 0) {
+				$("#brucontactnumarea").hide();
+				$("#nonbrucontactnumarea").hide();
+				$("#addcontactnumarea").hide();
+				$("#submitphonebutton").hide();
+     			}
+	     
+     			if($(this).val()=="Yes") {
+				$("#brucontactnumarea").fadeIn();
+				$("#nonbrucontactnumarea").hide();
+				$("#addcontactnumarea").fadeIn();
+				$("#submitphonebutton").fadeIn();
+				
+				document.getElementById("submitphonebutton").addEventListener("click", function() {
+  					if ( ($("#brucontactnumarea").val().length == 0 ) || ($("#addcontactnumarea").val().length == 0 ) ) {
 						$("#otherarea").hide();
 						alert("Please do not leave the required fields empty!");
 					}
 				
-					if ( ($("#Full-Name").val().length != 0) && ($("#Bru-HIMS-Number").val().length != 0) && ($("#Contact-Number").val().length != 0) &&
-					($("#District").val().length != 0) && ($("#Kampung").val().length != 0) && ($("#Jalan-Location").val().length != 0) &&
-					($("#Simpang-Number").val().length != 0) && ($("#House-Unit-Number").val().length != 0) ) {
-						$("#submitregistrationbutton").hide();
+					if ( ($("#brucontactnumarea").val().length != 0) && ($("#addcontactnumarea").val().length != 0) ) {
+						$("#submitphonebutton").hide();
+						$("#otherarea").fadeIn();
+					}
+				});
+     			}
+			
+     			if($(this).val()=="No") {
+				$("#brucontactnumarea").hide();
+				$("#nonbrucontactnumarea").fadeIn();
+				$("#addcontactnumarea").fadeIn();
+				$("#submitphonebutton").fadeIn();
+				
+				document.getElementById("submitphonebutton").addEventListener("click", function() {
+  					if ( ($("#nonbrucontactnumarea").val().length == 0 ) || ($("#addcontactnumarea").val().length == 0 ) ) {
+						$("#otherarea").hide();
+						alert("Please do not leave the required fields empty!");
+					}
+				
+					if ( ($("#nonbrucontactnumarea").val().length != 0) && ($("#addcontactnumarea").val().length != 0) ) {
+						$("#submitphonebutton").hide();
 						$("#otherarea").fadeIn();
 					}
 				});

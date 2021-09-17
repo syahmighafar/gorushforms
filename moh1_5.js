b07a003490b63860df673438e1e9535832e15e5e
syahmighafar <81664143+syahmighafar@users.noreply.github.com>
2021-09-17 10:22:37 +0800

    Update moh1_5.js

--- moh1_5.js
+++ moh1_5.js
@@ -13,7 +13,13 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		$("#passportnumarea").hide();
 		$("#submitregistrationbutton").hide();
 		$("#submitregistrationpassbutton").hide();
-		
+		$("#addpatientarea").hide();
+		$("#addpatientarea1").hide();
+		$("#patientcontrolarea").hide();
+		$("#completeaddpatient").hide();
+		$("#addpatientbutton").hide();
+		$("#removepatientbutton").hide();
+
 		document.getElementById("uploadbruhealthproof").required = false;
 		
 		document.getElementById("Type-Of-Mandatory").required = true;
@@ -91,7 +97,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   			if ( ($("#Full-Name").val().length == 0 ) || ($("#Bru-HIMS-Number").val().length == 0 ) || ($("#Contact-Number").val().length == 0) ||
 			($("#Date-of-Birth").val().length == 0 ) || ($("#IC-or-Passport").val().length == 0) || ($("#IC-Number").val().length == 0 ) ||
 			($("#Delivery-Address").val().length == 0) || ($("#Postal-Code").val().length == 0) ) {
-				$("#otherarea").hide();
+				$("#addpatientarea").hide();
 				alert("Please do not leave the required fields empty!");
 			}
 				
@@ -99,7 +105,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 			($("#Date-of-Birth").val().length != 0) && ($("#IC-or-Passport").val().length != 0) && ($("#IC-Number").val().length != 0) &&
 			($("#Delivery-Address").val().length != 0) && ($("#Postal-Code").val().length != 0)) {
 				$("#submitregistrationbutton").hide();
-				$("#otherarea").fadeIn();
+				$("#addpatientarea").fadeIn();
 			}
 		});
 		
@@ -107,7 +113,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   			if ( ($("#Full-Name").val().length == 0 ) || ($("#Bru-HIMS-Number").val().length == 0 ) || ($("#Contact-Number").val().length == 0) ||
 			($("#Date-of-Birth").val().length == 0 ) || ($("#IC-or-Passport").val().length == 0) || ($("#Passport-Number").val().length == 0 ) ||
 			($("#Delivery-Address").val().length == 0) || ($("#Postal-Code").val().length == 0) ) {
-				$("#otherarea").hide();
+				$("#addpatientarea").hide();
 				alert("Please do not leave the required fields empty!");
 			}
 				
@@ -115,10 +121,108 @@ document.addEventListener("DOMContentLoaded", function(event) {
 			($("#Date-of-Birth").val().length != 0) && ($("#IC-or-Passport").val().length != 0) && ($("#Passport-Number").val().length != 0) &&
 			($("#Delivery-Address").val().length != 0) && ($("#Postal-Code").val().length != 0)) {
 				$("#submitregistrationpassbutton").hide();
-				$("#otherarea").fadeIn();
+				$("#addpatientarea").fadeIn();
 			}
 		});
+		
+		$('#Additional-Patient').change(function(){
+			
+			if($(this).val().length == 0) {
 
+				$("#addpatientarea1").hide();
+				$("#patientcontrolarea").hide();
+				$("#completeaddpatient").hide();
+				$("#otherarea").hide();
+      			}
+	     
+      			if($(this).val()=="Yes") {
+
+				$("#addpatientarea1").fadeIn();
+				$("#patientcontrolarea").fadeIn();
+				$("#completeaddpatient").fadeIn();
+				$("#otherarea").hide();
+				
+				var additional_patient = document.getElementById('addpatientarea2');
+				countadd = 0;
+				
+				document.getElementById("addpatientbutton").addEventListener("click", function() {
+					var newheading = document.createElement('h4');
+					newheading.innerHTML = "Additional Patient " + (countadd+1);
+					additional_patient.appendChild(newheading);
+
+					var newDiv = document.createElement("div");
+					newDiv.setAttribute("id","fullnamearea"+ (countadd+1))
+					additional_patient.appendChild(newDiv);
+
+					var newLabel = document.createElement('label');
+					newLabel.innerHTML = "Full Name:";
+					document.getElementById("fullnamearea"+ (countadd+1)).appendChild(newLabel);
+
+					var newField = document.createElement('input');
+					newField.setAttribute('type','text');
+					newField.setAttribute('class','text-field w-input');
+					newField.setAttribute('maxlength','256');
+					newField.setAttribute('name','Full Name ' + (countadd+1));
+					newField.setAttribute('data-name','Full Name ' + (countadd+1));
+					newField.setAttribute('id','Full-Name'+ (countadd+1));
+					newField.setAttribute('placeholder','Full Name');
+					newField.required = true;
+					document.getElementById("fullnamearea"+ (countadd+1)).appendChild(newField);
+
+					var newDiv = document.createElement("div");
+					newDiv.setAttribute("id","bruhimsnumarea"+ (countadd+1))
+					additional_patient.appendChild(newDiv);
+
+					var newLabel = document.createElement('label');
+					newLabel.innerHTML = "Bru-HIMS Number:";
+					document.getElementById("bruhimsnumarea"+ (countadd+1)).appendChild(newLabel);
+
+					var newField = document.createElement('input');
+					newField.setAttribute('type','text');
+					newField.setAttribute('class','Bru HIMS Number');
+					newField.setAttribute('minlength','10');
+					newField.setAttribute('name','Bru-HIMS-Number-' + (countadd+1));
+					newField.setAttribute('data-name','Bru HIMS Number ' + (countadd+1));
+					newField.setAttribute('id','Bru-HIMS-Number-'+ (countadd+1));
+					newField.setAttribute('placeholder','BNxxxxxxxx');
+					newField.required = true;
+					document.getElementById("bruhimsnumarea"+ (countadd+1)).appendChild(newField);
+
+					countadd = countadd + 1;
+					
+					$("#removepatientbutton").fadeIn();
+				});
+				
+				document.getElementById("removepatientbutton").addEventListener("click", function() {
+					var div_tags = additional_patient.getElementsByTagName('div');
+					var heading_tags = additional_patient.getElementsByTagName('h4');
+					if(div_tags.length != 0) {
+						additional_patient.removeChild(div_tags[(div_tags.length) - 1]);
+						additional_patient.removeChild(div_tags[(div_tags.length) - 1]);
+						additional_patient.removeChild(heading_tags[(heading_tags.length) - 1]);
+						countadd = countadd - 1;
+						
+						if(div_tags.length == 0) {
+							$("#removepatientbutton").hide();
+						}
+					}
+				});
+				
+				document.getElementById("completeaddpatient").addEventListener("click", function() {
+  					$("#completeaddpatient").hide();
+					$("#otherarea").fadeIn();
+				});
+			}
+      		
+			if($(this).val()=="No") {
+
+				$("#addpatientarea1").hide();
+				$("#patientcontrolarea").hide();
+				$("#completeaddpatient").hide();
+				$("#otherarea").fadeIn();
+      			}
+    		});
+		
 		$('#Payment-Method').change(function(){
 			if($(this).val().length == 0) {
 				$("#paymentnoticearea").hide();

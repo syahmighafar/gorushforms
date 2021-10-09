1f7eef763bb9d8e9e92abae52f1320b9e7f0acbf
syahmighafar <81664143+syahmighafar@users.noreply.github.com>
2021-10-09 12:07:02 +0800

    Update moh_order_login.js

--- moh_order_login.js
+++ moh_order_login.js
@@ -1,33 +1,30 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-
 	$(document).ready(function(){
     
-    $("#uploadbruhealtharea").hide();
-    $("#deliverymethodarea").hide();
-    $("#selfcollectarea").hide();
-    $("#paymentmethodarea").hide();
-    $("#remarkarea").hide();
-    $("#asknotifyarea").hide();
-    $("#notifyoftenarea").hide();
-    $("#submitarea").hide();
-    
+		$("#uploadbruhealtharea").hide();
+		$("#deliverymethodarea").hide();
+		$("#selfcollectarea").hide();
+		$("#paymentmethodarea").hide();
+		$("#remarkarea").hide();
+		$("#asknotifyarea").hide();
+		$("#notifyoftenarea").hide();
+		$("#submitarea").hide();
+
 		$('input[name=radioQO]').change(function(){
 			if($(this).val().length == 0) {
 				$("#uploadbruhealtharea").hide();
-        $("#deliverymethodarea").hide();
-     	}
-	     
-     			if($(this).val()=="Yes") {
+				$("#deliverymethodarea").hide();
+			}
+
+			if($(this).val()=="Yes") {
 				$("#uploadbruhealtharea").fadeIn();
-        $("#deliverymethodarea").fadeIn();
-     			}
-		
-     			if($(this).val()=="No") {
+				$("#deliverymethodarea").fadeIn();
+			}
+
+			if($(this).val()=="No") {
 				$("#uploadbruhealtharea").hide();
-        $("#deliverymethodarea").fadeIn();
-     			}
+				$("#deliverymethodarea").fadeIn();
+			}
 		});
-		
-	});
-		
+	});		
 });

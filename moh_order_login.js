3bb4bb4b1385fab3057cb60949171e69a64774bd
syahmighafar <81664143+syahmighafar@users.noreply.github.com>
2021-10-09 15:50:16 +0800

    Update moh_order_login.js

--- moh_order_login.js
+++ moh_order_login.js
@@ -11,19 +11,65 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		$("#submitarea").hide();
 
 		$('input[name=radioQO]').change(function(){
-			if($(this).val().length == 0) {
+			if ($(this).val().length == 0) {
 				$("#uploadbruhealtharea").hide();
 				$("#deliverymethodarea").hide();
 			}
 
-			if($(this).val()=="Yes") {
+			if ( ($(this).val()=="No") || ($(this).val()=="Yes") ) {
 				$("#uploadbruhealtharea").fadeIn();
 				$("#deliverymethodarea").fadeIn();
 			}
+		});
+		
+		$('input[name=radioTOD]').change(function(){
+			if ($(this).val().length == 0) {
+				$("#selfcollectarea").hide();
+				$("#paymentmethodarea").hide();
+			}
 
-			if($(this).val()=="No") {
-				$("#uploadbruhealtharea").hide();
-				$("#deliverymethodarea").fadeIn();
+			if ( ($(this).val()=="Standard") || ($(this).val()=="Express") ) {
+				$("#selfcollectarea").hide();
+				$("#paymentmethodarea").fadeIn();
+			}
+			
+			if ($(this).val()=="Self Collect") {
+				$("#selfcollectarea").fadeIn();
+				$("#paymentmethodarea").hide();
+			}
+		});
+		
+		$('input[name=radioPAY]').change(function(){
+			if ($(this).val().length == 0) {
+				$("#remarkarea").hide();
+				$("#asknotifyarea").hide();
+			}
+
+			if ( ($(this).val()=="Cash") || ($(this).val()=="Bank Transfer") ) {
+				$("#remarkarea").fadeIn();
+				$("#asknotifyarea").fadeIn();
+			}
+		});
+		
+		$('input[name=radioNOTI]').change(function(){
+			if ($(this).val().length == 0) {
+				$("#notifyoftenarea").hide();
+				$("#submitarea").hide();
+			}
+
+			if ( ($(this).val()=="Cash") || ($(this).val()=="Bank Transfer") ) {
+				$("#notifyoftenarea").fadeIn();
+				$("#submitarea").hide();
+			}
+		});
+		
+		$('input[name=radioOFTEN]').change(function(){
+			if ($(this).val().length == 0) {
+				$("#submitarea").hide();
+			}
+
+			if ( ($(this).val().length != 0) ) {
+				$("#submitarea").fadeIn();
 			}
 		});
 	});		

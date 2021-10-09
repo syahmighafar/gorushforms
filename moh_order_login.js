a42fa954c4a57358ad0f34a0564f0e9981858796
syahmighafar <81664143+syahmighafar@users.noreply.github.com>
2021-10-09 12:01:13 +0800

    Create moh_order_login.js

---
+++ moh_order_login.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+	$(document).ready(function(){
+    
+    $("#uploadbruhealtharea").hide();
+    $("#deliverymethodarea").hide();
+    $("#selfcollectarea").hide();
+    $("#paymentmethodarea").hide();
+    $("#remarkarea").hide();
+    $("#asknotifyarea").hide();
+    $("#notifyoftenarea").hide();
+    $("#submitarea").hide();
+    
+		$('input[name=radioQO]').change(function(){
+			if($(this).val().length == 0) {
+				$("#uploadbruhealtharea").hide();
+        $("#deliverymethodarea").hide();
+     	}
+	     
+     			if($(this).val()=="Yes") {
+				$("#uploadbruhealtharea").fadeIn();
+        $("#deliverymethodarea").fadeIn();
+     			}
+		
+     			if($(this).val()=="No") {
+				$("#uploadbruhealtharea").hide();
+        $("#deliverymethodarea").fadeIn();
+     			}
+		});
+		
+	});
+		
+});

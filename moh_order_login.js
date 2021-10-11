d42d61b16ac24df141395a1dcbbae0787c0e4f9e
syahmighafar <81664143+syahmighafar@users.noreply.github.com>
2021-10-11 15:00:58 +0800

    Update moh_order_login.js

--- moh_order_login.js
+++ moh_order_login.js
@@ -2,51 +2,150 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	$(document).ready(function(){
     
 		$("#uploadbruhealtharea").hide();
+		$("#selecthcarea").hide();
+		$("#bmhcarea").hide();
+		$("#tutonghcarea").hide();
+		$("#belaithcarea").hide();
+		$("#temburonghcarea").hide();
 		$("#deliverymethodarea").hide();
 		$("#selfcollectarea").hide();
+		$("#selfcollectnextbutton").hide();
 		$("#paymentmethodarea").hide();
 		$("#remarkarea").hide();
 		$("#asknotifyarea").hide();
 		$("#notifyoftenarea").hide();
 		$("#submitarea").hide();
+		
+		
+		
 
 		$('input[name=radioQO]').change(function(){
 			if ($(this).val().length == 0) {
 				$("#uploadbruhealtharea").hide();
-				$("#deliverymethodarea").hide();
+				$("#selecthcarea").hide();
 			}
 
 			if ( ($(this).val()=="No") || ($(this).val()=="Yes") ) {
 				$("#uploadbruhealtharea").fadeIn();
-				$("#deliverymethodarea").fadeIn();
+				$("#selecthcarea").fadeIn();
 			}
+		});
+		
+		$('input[name=radioDistrict]').change(function(){
+			if ($(this).val().length == 0) {
+				$("#bmhcarea").hide();
+				$("#tutonghcarea").hide();
+				$("#belaithcarea").hide();
+				$("#temburonghcarea").hide();
+			}
+
+			if ($(this).val()=="Brunei Muara") {
+				$("#bmhcarea").fadeIn();
+				$("#tutonghcarea").hide();
+				$("#belaithcarea").hide();
+				$("#temburonghcarea").hide();
+			}
+			
+			if ($(this).val()=="Tutong") {
+				$("#bmhcarea").hide();
+				$("#tutonghcarea").fadeIn();
+				$("#belaithcarea").hide();
+				$("#temburonghcarea").hide();
+			}
+			
+			if ($(this).val()=="Belait") {
+				$("#bmhcarea").hide();
+				$("#tutonghcarea").hide();
+				$("#belaithcarea").fadeIn();
+				$("#temburonghcarea").hide();
+			}
+			
+			if ($(this).val()=="Temburong") {
+				$("#bmhcarea").hide();
+				$("#tutonghcarea").hide();
+				$("#belaithcarea").hide();
+				$("#temburonghcarea").fadeIn();
+			}
+		});
+		
+		$('#BNHC').change(function(){
+			if($(this).val().length == 0) {
+				$("#deliverymethodarea").hide();
+     			}
+	     
+     			if($(this).val().length != 0) {
+				$("#deliverymethodarea").fadeIn();
+     			}
+		});
+		
+		$('#TUHC').change(function(){
+			if($(this).val().length == 0) {
+				$("#deliverymethodarea").hide();
+     			}
+	     
+     			if($(this).val().length != 0) {
+				$("#deliverymethodarea").fadeIn();
+     			}
+		});
+		
+		$('#BHC').change(function(){
+			if($(this).val().length == 0) {
+				$("#deliverymethodarea").hide();
+     			}
+	     
+     			if($(this).val().length != 0) {
+				$("#deliverymethodarea").fadeIn();
+     			}
+		});
+		
+		$('#TEHC').change(function(){
+			if($(this).val().length == 0) {
+				$("#deliverymethodarea").hide();
+     			}
+	     
+     			if($(this).val().length != 0) {
+				$("#deliverymethodarea").fadeIn();
+     			}
 		});
 		
 		$('input[name=radioTOD]').change(function(){
 			if ($(this).val().length == 0) {
 				$("#selfcollectarea").hide();
 				$("#paymentmethodarea").hide();
+				$("#selfcollectnextbutton").hide();
 			}
 
-			if ( ($(this).val()=="Standard") || ($(this).val()=="Express") ) {
+			if ( ($(this).val()=="Standard") || ($(this).val()=="Express") || ($(this).val()=="Immediate") ) {
 				$("#selfcollectarea").hide();
 				$("#paymentmethodarea").fadeIn();
+				$("#selfcollectnextbutton").hide();
 			}
 			
 			if ($(this).val()=="Self Collect") {
 				$("#selfcollectarea").fadeIn();
 				$("#paymentmethodarea").hide();
+				$("#selfcollectnextbutton").fadeIn();
+			}
+		});
+		
+		document.getElementById("selfcollectnextbutton").addEventListener("click", function() {
+			if ($("#scDate").val().length == 0 ) {
+				$("#paymentmethodarea").hide();
+				alert("Please do not leave the required fields empty!");
+			}
+
+			if ($("#scDate").val().length != 0) {
+				$("#selfcollectnextbutton").hide();
+				$("#paymentmethodarea").fadeIn();
 			}
 		});
 		
 		$('input[name=radioPAY]').change(function(){
 			if ($(this).val().length == 0) {
-				$("#remarkarea").hide();
 				$("#asknotifyarea").hide();
 			}
 
 			if ( ($(this).val()=="Cash") || ($(this).val()=="Bank Transfer") ) {
-				$("#remarkarea").fadeIn();
 				$("#asknotifyarea").fadeIn();
 			}
 		});
@@ -54,22 +153,32 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		$('input[name=radioNOTI]').change(function(){
 			if ($(this).val().length == 0) {
 				$("#notifyoftenarea").hide();
+				$("#remarkarea").hide();
 				$("#submitarea").hide();
 			}
 
-			if ( ($(this).val()=="Cash") || ($(this).val()=="Bank Transfer") ) {
+			if ($(this).val()=="Yes") {
 				$("#notifyoftenarea").fadeIn();
+				$("#remarkarea").hide();
 				$("#submitarea").hide();
+			}
+			
+			if ($(this).val()=="No") {
+				$("#notifyoftenarea").fadeIn();
+				$("#remarkarea").fadeIn();
+				$("#submitarea").fadeIn();
 			}
 		});
 		
 		$('input[name=radioOFTEN]').change(function(){
 			if ($(this).val().length == 0) {
-				$("#submitarea").hide();
+				$("#remarkarea").hide();
+				$("#submitbutton").hide();
 			}
 
 			if ( ($(this).val().length != 0) ) {
-				$("#submitarea").fadeIn();
+				$("#remarkarea").fadeIn();
+				$("#submitbutton").fadeIn();
 			}
 		});
 	});		

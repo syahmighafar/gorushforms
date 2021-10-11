d742ff46b611bb4201bfa6c69636e7a099b86e64
syahmighafar <81664143+syahmighafar@users.noreply.github.com>
2021-10-11 16:28:55 +0800

    Create phc_order_login.js

---
+++ phc_order_login.js
@@ -0,0 +1,112 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+	$(document).ready(function(){
+    
+		$("#uploadbruhealtharea").hide();
+		$("#deliverymethodarea").hide();
+		$("#selfcollectarea").hide();
+		$("#selfcollectnextbutton").hide();
+		$("#paymentmethodarea").hide();
+		$("#remarkarea").hide();
+		$("#asknotifyarea").hide();
+		$("#notifyoftenarea").hide();
+		$("#submitbutton").hide();
+
+		$('input[name=radioQO]').change(function(){
+			if ($(this).val().length == 0) {
+				$("#uploadbruhealtharea").hide();
+				$("#deliverymethodarea").hide();
+			}
+
+			if ( ($(this).val()=="No") || ($(this).val()=="Yes") ) {
+				$("#uploadbruhealtharea").fadeIn();
+				$("#deliverymethodarea").fadeIn();
+			}
+		});
+		
+		$('input[name=radioTOD]').change(function(){
+			if ($(this).val().length == 0) {
+				$("#selfcollectarea").hide();
+				$("#paymentmethodarea").hide();
+				$("#selfcollectnextbutton").hide();
+				
+				document.getElementById('scDate').value = '';
+			}
+
+			if ( ($(this).val()=="Standard") ) {
+				$("#selfcollectarea").hide();
+				$("#paymentmethodarea").fadeIn();
+				$("#selfcollectnextbutton").hide();
+				
+				document.getElementById('scDate').value = '';
+			}
+			
+			if ($(this).val()=="Self Collect") {
+				$("#selfcollectarea").fadeIn();
+				$("#paymentmethodarea").hide();
+				$("#selfcollectnextbutton").fadeIn();
+				
+				document.getElementById('scDate').value = '';
+			}
+		});
+		
+		document.getElementById("selfcollectnextbutton").addEventListener("click", function() {
+			if ($("#scDate").val().length == 0 ) {
+				$("#paymentmethodarea").hide();
+				alert("Please do not leave the required fields empty!");
+			}
+
+			if ($("#scDate").val().length != 0) {
+				$("#selfcollectnextbutton").hide();
+				$("#paymentmethodarea").fadeIn();
+			}
+		});
+		
+		$('input[name=radioPAY]').change(function(){
+			if ($(this).val().length == 0) {
+				$("#asknotifyarea").hide();
+			}
+
+			if ( ($(this).val()=="Cash") || ($(this).val()=="Bank Transfer") ) {
+				$("#asknotifyarea").fadeIn();
+			}
+		});
+		
+		$('input[name=radioNOTI]').change(function(){
+			if ($(this).val().length == 0) {
+				$("#notifyoftenarea").hide();
+				$("#remarkarea").hide();
+				$("#submitbutton").hide();
+				
+				$('input[name=radioOFTEN]').prop('checked', false);
+			}
+
+			if ($(this).val()=="Yes") {
+				$("#notifyoftenarea").fadeIn();
+				$("#remarkarea").hide();
+				$("#submitbutton").hide();
+				
+				$('input[name=radioOFTEN]').prop('checked', false);
+			}
+			
+			if ($(this).val()=="No") {
+				$("#notifyoftenarea").hide();
+				$("#remarkarea").fadeIn();
+				$("#submitbutton").fadeIn();
+				
+				$('input[name=radioOFTEN]').prop('checked', false);
+			}
+		});
+		
+		$('input[name=radioOFTEN]').change(function(){
+			if ($(this).val().length == 0) {
+				$("#remarkarea").hide();
+				$("#submitbutton").hide();
+			}
+
+			if ( ($(this).val().length != 0) ) {
+				$("#remarkarea").fadeIn();
+				$("#submitbutton").fadeIn();
+			}
+		});
+	});		
+});

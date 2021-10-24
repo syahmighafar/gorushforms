8fdc1778f43d54c7cb4504f67c824b592a8be55d
syahmighafar <81664143+syahmighafar@users.noreply.github.com>
2021-10-24 20:10:34 +0800

    Update moh_order_login.js

--- moh_order_login.js
+++ moh_order_login.js
@@ -10,6 +10,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		$("#clinicAddress").hide();
 		$("#deliverymethodarea").hide();
 		
+		$("#expressArea").hide();
+		$("#immediateArea").hide();
+		
 		$("#pricedurationarea").hide();
 		$("#stdselfPrice").hide();
 		$("#expPrice").hide();
@@ -53,6 +56,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		$('input[name=radioDistrict]').change(function(){
 			if ($(this).val().length == 0) {
 				
+				$("#expressArea").hide();
+				$("#immediateArea").hide();
+				
 				document.getElementById("mapSource").src="";
 				$("#hcAreaMapAddress").hide();
 				$("#clinicAddress").hide();
@@ -69,6 +75,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
 			}
 
 			if ($(this).val()=="Brunei Muara") {
+				
+				$("#expressArea").fadeIn();
+				$("#immediateArea").fadeIn();
 				
 				document.getElementById("mapSource").src="";
 				$("#hcAreaMapAddress").hide();
@@ -87,6 +96,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
 			
 			if ($(this).val()=="Tutong") {
 				
+				$("#expressArea").hide();
+				$("#immediateArea").hide();
+				
 				document.getElementById("mapSource").src="";
 				$("#hcAreaMapAddress").hide();
 				$("#clinicAddress").hide();
@@ -104,6 +116,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
 			
 			if ($(this).val()=="Belait") {
 				
+				$("#expressArea").hide();
+				$("#immediateArea").hide();
+				
 				document.getElementById("mapSource").src="";
 				$("#hcAreaMapAddress").hide();
 				$("#clinicAddress").hide();
@@ -120,6 +135,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
 			}
 			
 			if ($(this).val()=="Temburong") {
+				
+				$("#expressArea").hide();
+				$("#immediateArea").hide();
 				
 				document.getElementById("mapSource").src="";
 				$("#hcAreaMapAddress").hide();
@@ -139,6 +157,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		
 		$('#BNHC').change(function(){
 			if($(this).val().length == 0) {
+				$("#immediateArea").hide();
 				$("#deliverymethodarea").hide();
 				document.getElementById("mapSource").src="";
 				$("#hcAreaMapAddress").hide();
@@ -146,6 +165,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
      			}
 			
 			if($(this).val()=="Raja Isteri Pengiran Anak Saleha Hospital") {
+				$("#immediateArea").fadeIn();
 				$("#clinicAddress").hide();
 				$("#deliverymethodarea").fadeIn();
 				document.getElementById("mapSource").src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3975.2805202346863!2d114.93130540523119!3d4.892630019678778!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x32228acb18b46725%3A0x9118155b10d353ca!2sRaja%20Isteri%20Pengiran%20Anak%20Saleha%20Hospital!5e0!3m2!1sen!2sbn!4v1634281650713!5m2!1sen!2sbn";
@@ -153,6 +173,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
      			}
 			
 			if($(this).val()=="Pengkalan Batu Health Centre") {
+				$("#immediateArea").hide();
 				$("#clinicAddress").hide();
 				$("#deliverymethodarea").fadeIn();
 				document.getElementById("mapSource").src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3975.9930134353094!2d114.81397081532558!3d4.771159642369682!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x322263643d7b9bf9%3A0xab86d8cb5835ceda!2sPengkalan%20Batu%20Health%20Centre!5e0!3m2!1sen!2sbn!4v1634285210387!5m2!1sen!2sbn";
@@ -161,6 +182,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 			
 			
 			if($(this).val()=="Jubli Perak Sengkurong Health Centre") {
+				$("#immediateArea").hide();
 				$("#clinicAddress").hide();
 				$("#deliverymethodarea").fadeIn();
 				document.getElementById("mapSource").src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3975.2877506842783!2d114.84314291532539!3d4.891412441334922!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x32226084b479a23b%3A0xf3137ef638ed3cfd!2sJubli%20Perak%20Sengkurong%20Health%20Centre!5e0!3m2!1sen!2sbn!4v1634285543314!5m2!1sen!2sbn";
@@ -168,6 +190,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
      			}
 
 			if($(this).val()=="Jubli Emas Kg Perpindahan Bunut Health Centre") {
+				$("#immediateArea").hide();
 				$("#clinicAddress").hide();
 				$("#deliverymethodarea").fadeIn();
 				document.getElementById("mapSource").src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3975.547397189751!2d114.88291741532552!3d4.847486841715639!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x322261c61f38a619%3A0x3a6a37bd46269d3a!2sJubli%20Emas%20Health%20Centre!5e0!3m2!1sen!2sbn!4v1634285599556!5m2!1sen!2sbn";
@@ -175,6 +198,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
      			}
 
 			if($(this).val()=="Pengiran Anak Puteri Hajah Rashidah Sa'adatul Bolkiah Health Centre") {
+				$("#immediateArea").hide();
 				$("#clinicAddress").hide();
 				$("#deliverymethodarea").fadeIn();
 				document.getElementById("mapSource").src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3975.3769360804904!2d114.9340483153255!3d4.876369041465689!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x32228ac25aec45df%3A0x8707ccd2be1d106b!2sPAPHRSB%20SG%20ASAM%20HEALTH%20CENTRE!5e0!3m2!1sen!2sbn!4v1634285838280!5m2!1sen!2sbn";
@@ -182,6 +206,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
      			}
 			
 			if($(this).val()=="Pengiran Anak Puteri Hajah Muta-Wakillah Hayatul Bolkiah Health Centre") {
+				$("#immediateArea").hide();
 				$("#clinicAddress").hide();
 				$("#deliverymethodarea").fadeIn();
 				document.getElementById("mapSource").src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3975.0479626441015!2d114.90431041532548!3d4.9316318409836315!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3222f57fa2c50b8f%3A0x5e99dc6cafc60b21!2sPengiran%20Anak%20Puteri%20Hajah%20Muta-Wakillah%20Hayatul%20Bolkiah%20Health%20Centre!5e0!3m2!1sen!2sbn!4v1634285881919!5m2!1sen!2sbn";
@@ -189,6 +214,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
      			}
 			
 			if($(this).val()=="Berakas Health Centre") {
+				$("#immediateArea").hide();
 				$("#clinicAddress").hide();
 				$("#deliverymethodarea").fadeIn();
 				document.getElementById("mapSource").src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3974.828175459776!2d114.9636824153256!3d4.96821124066185!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3222f42787932131%3A0xf9ab86325b87674!2sPusat%20Kesihatan%20Berakas!5e0!3m2!1sen!2sbn!4v1634285920206!5m2!1sen!2sbn";
@@ -196,13 +222,23 @@ document.addEventListener("DOMContentLoaded", function(event) {
      			}
 
 			if($(this).val()=="Muara Health Centre") {
+				$("#immediateArea").hide();
 				$("#clinicAddress").hide();
 				$("#deliverymethodarea").fadeIn();
 				document.getElementById("mapSource").src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3974.4576007276696!2d115.06447081532535!3d5.029285440119686!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3222f06dcb40542b%3A0xdce8b6a5b55c836d!2sMuara%20Health%20Centre!5e0!3m2!1sen!2sbn!4v1634285963322!5m2!1sen!2sbn";
 				$("#hcAreaMapAddress").fadeIn();	
      			}
+			
+			if($(this).val()=="Psychiatry Department, Ministry of Health") {
+				$("#immediateArea").hide();
+				$("#clinicAddress").hide();
+				$("#deliverymethodarea").fadeIn();
+				document.getElementById("mapSource").src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3975.254386052767!2d114.90992181532567!3d4.897028391286052!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3222f553a7c821df%3A0x65c23364c25df9!2sPsychiatry%20Department%2C%20Ministry%20of%20Health!5e0!3m2!1sen!2sbn!4v1634992947581!5m2!1sen!2sbn";
+				$("#hcAreaMapAddress").fadeIn();	
+     			}
 
 			if($(this).val()=="KG Bolkiah") {
+				$("#immediateArea").hide();
 				$("#deliverymethodarea").fadeIn();
 				document.getElementById("mapSource").src="";
 				$("#hcAreaMapAddress").hide();
@@ -210,6 +246,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
      			}
 
 			if($(this).val()=="SG Bunga") {
+				$("#immediateArea").hide();
 				$("#clinicAddress").hide();
 				$("#deliverymethodarea").fadeIn();
 				document.getElementById("mapSource").src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3975.1432004986946!2d115.01193264313245!3d4.915696991808234!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3222f36f1f0926d9%3A0x5eb0de579e4e2269!2sSungai%20Bunga%20Health%20Clinic%2C%20Kampong%20Sungai%20Bunga!5e0!3m2!1sen!2sbn!4v1634286214099!5m2!1sen!2sbn";

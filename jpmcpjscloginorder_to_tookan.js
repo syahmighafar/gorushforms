968106f3e0e06bca575af5d9373a73996a7643fe
syahmighafar <81664143+syahmighafar@users.noreply.github.com>
2021-11-08 13:40:03 +0800

    Update jpmcpjscloginorder_to_tookan.js

--- jpmcpjscloginorder_to_tookan.js
+++ jpmcpjscloginorder_to_tookan.js
@@ -12,10 +12,10 @@ document.addEventListener("submit", function(event) {
   
   var customerAddress = "";
   
-  if (document.getElementById("address_3").value.length == 0){
+  if (document.getElementById("Simpang").value.length == 0){
     customerAddress = document.getElementById("address_1").value + ", " + document.getElementById("address_2").value + ", " + document.getElementById("address_4").value;
   } else{
-    customerAddress = document.getElementById("address_1").value + ", " + document.getElementById("address_2").value + ", " + document.getElementById("address_3").value
+    customerAddress = document.getElementById("address_1").value + ", " + document.getElementById("address_2").value + ", " + document.getElementById("Simpang").value
    + ", " + document.getElementById("address_4").value;
   }
   var jobdeliverydatetime = "";

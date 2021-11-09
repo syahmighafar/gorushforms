029452e20cb8415e1c2f3499b919b90e67d3bcef
syahmighafar <81664143+syahmighafar@users.noreply.github.com>
2021-11-09 16:44:22 +0800

    Update phc_to_tookan.js

--- phc_to_tookan.js
+++ phc_to_tookan.js
@@ -67,6 +67,7 @@ document.addEventListener("submit", function(event) {
     let customerIC = document.getElementById("icNumber").value;
   let customerRemarks = document.getElementById("re").value;
   let customerPM = document.querySelector('input[name=radioPAY]:checked').value
+  let order_Origin = document.getElementById("orderOrigin").value;
 
   var request = new XMLHttpRequest();
 
@@ -96,7 +97,7 @@ document.addEventListener("submit", function(event) {
     'job_delivery_datetime': jobdeliverydatetime,
     'custom_field_template': 'Local_Delivery',
       'meta_data': [{"label":"IC","data":customerIC},{"label":"BRUHIMS","data":orderId},{"label":"Remarks","data":customerRemarks},
-      {"label":"Type","data":jobDescription},{"label":"Method","data":customerPM},{"label":"Barcode","data":orderId}],
+      {"label":"Type","data":jobDescription},{"label":"Method","data":customerPM},{"label":"Barcode","data":orderId},{"label":"Order Origin","data":order_Origin}],
     'team_id': '921691',
     'auto_assignment': '0',
     'has_pickup': '0',

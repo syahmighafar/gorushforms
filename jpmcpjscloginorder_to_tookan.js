d29d90b12e2a177e712f5f2ea0b24355297e98dd
syahmighafar <81664143+syahmighafar@users.noreply.github.com>
2021-11-09 16:30:32 +0800

    Update jpmcpjscloginorder_to_tookan.js

--- jpmcpjscloginorder_to_tookan.js
+++ jpmcpjscloginorder_to_tookan.js
@@ -3,6 +3,40 @@ document.addEventListener("submit", function(event) {
   event.preventDefault();
   
   $(document).ready(function(){
+    
+    var today = new Date();
+    var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
+    
+    var ampm = '';
+    var ampmhour = '';
+    var ampmmin = '';
+    
+    if (today.getHours()<12){
+      ampm = 'am';
+      ampmhour = (today.getHours());
+    }
+
+    if (today.getHours()==12){
+      ampm = 'pm';
+      ampmhour = (today.getHours());
+    }
+
+    if (today.getHours()>12){
+      ampm = 'pm';
+      ampmhour = (today.getHours() - 12);
+    }
+
+    if (today.getMinutes()<10){
+      ampmmin = "0" + (today.getMinutes());
+    }
+
+    if (today.getMinutes()>=10){
+      ampmmin = (today.getMinutes());
+    }
+    var time = ampmhour + ":" + ampmmin + " " + ampm;
+
+    var dateTime = date+' '+time;
+    document.getElementById("dateSubmitted").value = dateTime;
   
   let orderId = document.getElementById("jpmc").value;
   let jobDescription = document.querySelector('input[name=radioTOD]:checked').value

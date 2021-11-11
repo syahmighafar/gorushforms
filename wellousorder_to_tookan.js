08e1f2e42d34e085d7880df25e1a32de9a45aef3
syahmighafar <81664143+syahmighafar@users.noreply.github.com>
2021-11-11 10:37:40 +0800

    Update wellousorder_to_tookan.js

--- wellousorder_to_tookan.js
+++ wellousorder_to_tookan.js
@@ -3,19 +3,20 @@ document.addEventListener("submit", function(event) {
   event.preventDefault();
   
   $(document).ready(function(){
+    
+  let agentName = document.getElementById("Agent-Name").value; 
   
-  let orderId = document.getElementById("bruhims").value;
-  let jobDescription = document.querySelector('input[name=TypeofDelivery]:checked').value
-  let customerEmail = document.getElementById("id-Email").value;
-  let customerUsername = document.getElementById("name").value;
-  let customerPhone = document.getElementById("contact_1").value;
+  let wellousItems = document.getElementById("Items").value;
+  let receiverName = document.getElementById("name").value;
+  let receiverPhone = document.getElementById("contact_1").value;
+  let jobDescription = document.getElementById("Delivery-Type").value;
   
-  var customerAddress = "";
+  var receiverAddress = "";
   
   if (document.getElementById("Simpang").value.length == 0){
-    customerAddress = document.getElementById("address_1").value + ", " + document.getElementById("address_2").value + ", " + document.getElementById("address_4").value;
+    receiverAddress = document.getElementById("address_1").value + ", " + document.getElementById("address_2").value + ", " + document.getElementById("address_4").value;
   } else{
-    customerAddress = document.getElementById("address_1").value + ", " + document.getElementById("address_2").value + ", " + document.getElementById("Simpang").value
+    receiverAddress = document.getElementById("address_1").value + ", " + document.getElementById("address_2").value + ", " + document.getElementById("Simpang").value
    + ", " + document.getElementById("address_4").value;
   }
   var jobdeliverydatetime = "";
@@ -30,9 +31,7 @@ document.addEventListener("submit", function(event) {
     var time = "17:00:00";
   jobdeliverydatetime = date+' '+time;
     
-    let customerIC = document.getElementById("icNumber").value;
-  let customerRemarks = document.getElementById("re").value;
-  let customerPM = document.querySelector('input[name=PaymentMethod]:checked').value
+  let customerPM = document.getElementById("Payment-Method").value;
   let order_Origin = document.getElementById("orderOrigin").value;
 
   var request = new XMLHttpRequest();
@@ -46,24 +45,23 @@ document.addEventListener("submit", function(event) {
       console.log('Status:', this.status);
       console.log('Headers:', this.getAllResponseHeaders());
       console.log('Body:', this.responseText);
-      document.getElementById("mohguestform").submit();
+      document.getElementById("wf-form-Wellous-Order-Form").submit();
     }
   };
 
   var body = {
     'api_key': '51676580f24b091114132d38111925401ee4c2f328d978375e1f03',
-    'order_id': orderId,
+    'order_id': '',
     'job_description': jobDescription,
-    'customer_email': customerEmail,
-    'customer_username': customerUsername,
-    'customer_phone': customerPhone,
-    'customer_address': customerAddress,
+    'customer_email': '',
+    'customer_username': receiverName,
+    'customer_phone': receiverPhone,
+    'customer_address': receiverAddress,
     'latitude': '',
     'longitude': '',
     'job_delivery_datetime': jobdeliverydatetime,
-    'custom_field_template': 'Local_Delivery',
-      'meta_data': [{"label":"IC","data":customerIC},{"label":"BRUHIMS","data":orderId},{"label":"Remarks","data":customerRemarks},
-      {"label":"Type","data":jobDescription},{"label":"Method","data":customerPM},{"label":"Barcode","data":orderId},{"label":"OrderOriginFrom","data":order_Origin}],
+    'custom_field_template': 'Wellous_Delivery',
+      'meta_data': [{"label":"Agent_Name","data":agentName},{"label":"Products","data":wellousItems},{"label":"Method","data":customerPM},{"label":"OrderOriginFrom","data":order_Origin}],
     'team_id': '921691',
     'auto_assignment': '0',
     'has_pickup': '0',

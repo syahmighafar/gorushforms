873c4e2f6e91eac2fa7483507bdc11d74e070e2b
syahmighafar <81664143+syahmighafar@users.noreply.github.com>
2021-11-23 17:05:28 +0800

    Update mohguestform.js

--- mohguestform.js
+++ mohguestform.js
@@ -1,6 +1,40 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     $(document).ready(function () {
         
+        var today = new Date();
+        var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
+
+        var ampm = '';
+        var ampmhour = '';
+        var ampmmin = '';
+
+        if (today.getHours()<12){
+          ampm = 'am';
+          ampmhour = (today.getHours());
+        }
+
+        if (today.getHours()==12){
+          ampm = 'pm';
+          ampmhour = (today.getHours());
+        }
+
+        if (today.getHours()>12){
+          ampm = 'pm';
+          ampmhour = (today.getHours() - 12);
+        }
+
+        if (today.getMinutes()<10){
+          ampmmin = "0" + (today.getMinutes());
+        }
+
+        if (today.getMinutes()>=10){
+          ampmmin = (today.getMinutes());
+        }
+        var time = ampmhour + ":" + ampmmin + " " + ampm;
+
+        var dateTime = date+' '+time;
+        document.getElementById("dateSubmitted").value = dateTime;
+        
         var todChoice = 0;
 
         const d = new Date();

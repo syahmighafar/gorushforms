6774806173a91b4ae293077f6cc020e6daf83add
syahmighafar <81664143+syahmighafar@users.noreply.github.com>
2021-11-25 10:19:02 +0800

    Update phcloginorder_test.js

--- phcloginorder_test.js
+++ phcloginorder_test.js
@@ -147,40 +147,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
         $('#Agreement').change(function () {
             if ($(this).val().checked = true) {
 
-                var today = new Date();
-                var date = today.getDate() + '-' + (today.getMonth() + 1) + '-' + today.getFullYear();
-
-                var ampm = '';
-                var ampmhour = '';
-                var ampmmin = '';
-
-                if (today.getHours() < 12) {
-                    ampm = 'am';
-                    ampmhour = (today.getHours());
-                }
-
-                if (today.getHours() == 12) {
-                    ampm = 'pm';
-                    ampmhour = (today.getHours());
-                }
-
-                if (today.getHours() > 12) {
-                    ampm = 'pm';
-                    ampmhour = (today.getHours() - 12);
-                }
-
-                if (today.getMinutes() < 10) {
-                    ampmmin = "0" + (today.getMinutes());
-                }
-
-                if (today.getMinutes() >= 10) {
-                    ampmmin = (today.getMinutes());
-                }
-                var time = ampmhour + ":" + ampmmin + " " + ampm;
-
-                var dateTime = date + ' ' + time;
-                document.getElementById("dateSubmitted").value = dateTime;
-
                 $("#submitbutton").fadeIn();
             }
         });

c14ef167e07e542c89ba47bd3bf4e58f6b456415
syahmighafar <81664143+syahmighafar@users.noreply.github.com>
2021-11-25 08:40:10 +0800

    Create phcloginorder_test.js

---
+++ phcloginorder_test.js
@@ -0,0 +1,188 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    $(document).ready(function () {
+
+        $("#deliverymethodarea").hide();
+
+        $("#pricedurationarea").hide();
+        $("#stdselfPrice").hide();
+        $("#expPrice").hide();
+        $("#immPrice").hide();
+        $("#stdDuration").hide();
+        $("#expDuration").hide();
+        $("#immDuration").hide();
+
+        $("#stdexpselfMessage").hide();
+        $("#immMessage").hide();
+
+        $("#selfcollectarea").hide();
+        $("#selfcollectnextbutton").hide();
+        $("#paymentmethodarea").hide();
+
+        $("#paymentnoticearea").hide();
+
+        $("#remarkarea").hide();
+
+        $("#agreementArea").hide();
+        $("#stdAgree").hide();
+        $("#expAgree").hide();
+        $("#immAgree").hide();
+
+        $("#pleasewait").hide();
+
+        $("#submitbutton").hide();
+
+        $('input[name=radioQO]').change(function () {
+            if ($(this).val().length == 0) {
+                $("#deliverymethodarea").hide();
+            }
+
+            if (($(this).val() == "No") || ($(this).val() == "Yes")) {
+                $("#deliverymethodarea").fadeIn();
+            }
+        });
+
+        $('input[name=radioTOD]').change(function () {
+            if ($(this).val().length == 0) {
+                $("#selfcollectarea").hide();
+                $("#paymentmethodarea").hide();
+                $("#selfcollectnextbutton").hide();
+
+                $("#pricedurationarea").hide();
+                $("#stdselfPrice").hide();
+                $("#expPrice").hide();
+                $("#immPrice").hide();
+                $("#stdDuration").hide();
+                $("#expDuration").hide();
+                $("#immDuration").hide();
+
+                $("#stdexpselfMessage").hide();
+                $("#immMessage").hide();
+
+                $("#stdAgree").hide();
+                $("#expAgree").hide();
+                $("#immAgree").hide();
+
+                document.getElementById('scDate').value = '';
+            }
+
+            if ($(this).val() == "Standard ($3)") {
+                $("#selfcollectarea").hide();
+                $("#paymentmethodarea").fadeIn();
+                $("#selfcollectnextbutton").hide();
+
+                $("#pricedurationarea").fadeIn();
+                $("#stdselfPrice").fadeIn();
+                $("#expPrice").hide();
+                $("#immPrice").hide();
+                $("#stdDuration").fadeIn();
+                $("#expDuration").hide();
+                $("#immDuration").hide();
+
+                $("#stdexpselfMessage").fadeIn();
+                $("#immMessage").hide();
+
+                $("#stdAgree").fadeIn();
+                $("#expAgree").hide();
+                $("#immAgree").hide();
+
+                document.getElementById('scDate').value = '';
+            }
+
+            if ($(this).val() == "Self Collect") {
+                $("#selfcollectarea").fadeIn();
+                $("#paymentmethodarea").hide();
+                $("#selfcollectnextbutton").fadeIn();
+
+                $("#pricedurationarea").fadeIn();
+                $("#stdselfPrice").fadeIn();
+                $("#expPrice").hide();
+                $("#immPrice").hide();
+                $("#stdDuration").hide();
+                $("#expDuration").hide();
+                $("#immDuration").hide();
+
+                $("#stdexpselfMessage").fadeIn();
+                $("#immMessage").hide();
+
+                $("#stdAgree").hide();
+                $("#expAgree").hide();
+                $("#immAgree").hide();
+
+                document.getElementById('scDate').value = '';
+            }
+        });
+
+        document.getElementById("selfcollectnextbutton").addEventListener("click", function () {
+            if ($("#scDate").val().length == 0) {
+                $("#paymentmethodarea").hide();
+                alert("Please do not leave the required fields empty!");
+            }
+
+            if ($("#scDate").val().length != 0) {
+                $("#selfcollectnextbutton").hide();
+                $("#paymentmethodarea").fadeIn();
+            }
+        });
+
+        $('input[name=radioPAY]').change(function () {
+            if ($(this).val().length == 0) {
+                $("#paymentnoticearea").hide();
+                $("#remarkarea").hide();
+                $("#agreementArea").hide();
+            }
+
+            if ($(this).val() == "Cash") {
+                $("#paymentnoticearea").hide();
+                $("#remarkarea").fadeIn();
+                $("#agreementArea").fadeIn();
+            }
+
+            if ($(this).val() == "Bank Transfer") {
+                $("#paymentnoticearea").fadeIn();
+                $("#remarkarea").fadeIn();
+                $("#agreementArea").fadeIn();
+            }
+        });
+
+        $('#Agreement').change(function () {
+            if ($(this).val().checked = true) {
+
+                var today = new Date();
+                var date = today.getDate() + '-' + (today.getMonth() + 1) + '-' + today.getFullYear();
+
+                var ampm = '';
+                var ampmhour = '';
+                var ampmmin = '';
+
+                if (today.getHours() < 12) {
+                    ampm = 'am';
+                    ampmhour = (today.getHours());
+                }
+
+                if (today.getHours() == 12) {
+                    ampm = 'pm';
+                    ampmhour = (today.getHours());
+                }
+
+                if (today.getHours() > 12) {
+                    ampm = 'pm';
+                    ampmhour = (today.getHours() - 12);
+                }
+
+                if (today.getMinutes() < 10) {
+                    ampmmin = "0" + (today.getMinutes());
+                }
+
+                if (today.getMinutes() >= 10) {
+                    ampmmin = (today.getMinutes());
+                }
+                var time = ampmhour + ":" + ampmmin + " " + ampm;
+
+                var dateTime = date + ' ' + time;
+                document.getElementById("dateSubmitted").value = dateTime;
+
+                $("#submitbutton").fadeIn();
+            }
+        });
+    });
+});

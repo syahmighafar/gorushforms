10d8b87c12c9d13e15fa72da087b161b20d84fbe
syahmighafar <81664143+syahmighafar@users.noreply.github.com>
2021-12-20 10:18:51 +0800

    Update phcguestform.js

--- phcguestform.js
+++ phcguestform.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     $(document).ready(function () {
+    	var countAgree = 0;
 
         $("#requesterarea").hide();
         $("#requesternextbutton").hide();
@@ -300,7 +301,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
         });
 
         $('#Agreement').change(function () {
-            if ($(this).val().checked = true) {
+            if (countAgree == 0){
+                countAgree = countAgree + 1;
 
                 $("#pleasewait").fadeIn();
 

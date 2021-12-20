737a67a71e6f6b32d95aa8f2a7dc6ac6d00c4a2c
syahmighafar <81664143+syahmighafar@users.noreply.github.com>
2021-12-20 16:49:28 +0800

    Update mohguestform.js

--- mohguestform.js
+++ mohguestform.js
@@ -813,15 +813,11 @@ document.addEventListener("DOMContentLoaded", function (event) {
 								json_responseo = JSON.parse(responseo);
 
 								var counttaskhistory = json_responseo.data["length"];
-								var checkPatientOrderId = "";
 
 								for (let i = 0; i < counttaskhistory; i++) {
-									if (json_responseo.data[i].custom_field["length"] = 7) {
-										checkPatientOrderId = json_responseo.data[i].custom_field[6].data;
-										if (checkPatientOrderId = patientOrderId) {
-											document.getElementById("Tookan-Tracking").value = json_responseo.data[i].job_id;
-											i = counttaskhistory;
-										}
+									if (json_responseo.data[i].custom_field[6].data == patientOrderId) {
+										document.getElementById("Tookan-Tracking").value = json_responseo.data[i].job_id;
+										i = counttaskhistory;
 									}
 								}
 

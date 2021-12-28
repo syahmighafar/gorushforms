d87aabda8b07a9507e67e6be2c3d2fd56b50c294
syahmighafar <81664143+syahmighafar@users.noreply.github.com>
2021-12-28 17:07:09 +0800

    Update returnform_to_tookan.js

--- returnform_to_tookan.js
+++ returnform_to_tookan.js
@@ -73,14 +73,26 @@ document.addEventListener("DOMContentLoaded", function (event) {
                 var jobdeliverydatetime = "";
                 var todaysDate = new Date();
 
-                if (todaysDate.getDate() >= 28) {
-                    var date = todaysDate.getFullYear() + '-' + (todaysDate.getMonth() + 2) + '-' + 28;
-                } else {
-                    var date = todaysDate.getFullYear() + '-' + (todaysDate.getMonth() + 1) + '-' + 28;
+                if (todaysDate.getMonth() < 11) {
+                    if (todaysDate.getDate() < 28) {
+                        var dateD = todaysDate.getFullYear() + '-' + (todaysDate.getMonth() + 1) + '-28';
+                    }
+                    if (todaysDate.getDate() >= 28) {
+                        var dateD = todaysDate.getFullYear() + '-' + (todaysDate.getMonth() + 2) + '-28';
+                    }
                 }
 
-                var time = "17:00:00";
-                jobdeliverydatetime = date + ' ' + time;
+                if (todaysDate.getMonth() >= 11) {
+                    if (todaysDate.getDate() < 28) {
+                        var dateD = (todaysDate.getFullYear()) + '-12-28';
+                    }
+                    if (todaysDate.getDate() >= 28) {
+                        var dateD = (todaysDate.getFullYear() + 1) + '-01-28';
+                    }
+                }
+
+                var timeD = "17:00:00";
+                jobdeliverydatetime = dateD + ' ' + timeD;
 
                 var request = new XMLHttpRequest();
 

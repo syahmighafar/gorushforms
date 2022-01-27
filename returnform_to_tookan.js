3cd8dd99bf5cb89394bbbd674b5357dbc2975c67
syahmighafar <81664143+syahmighafar@users.noreply.github.com>
2022-01-27 13:30:18 +0800

    Update returnform_to_tookan.js

--- returnform_to_tookan.js
+++ returnform_to_tookan.js
@@ -112,10 +112,10 @@ document.addEventListener("DOMContentLoaded", function (event) {
                         var dateD = todaysDate.getFullYear() + '-' + (todaysDate.getMonth() + 1) + '-31';
                     }
                     if (todaysDate.getDate() >= 31) {
-                        if (todaysDate.getHours() < 17) {
+                        if (todaysDate.getHours() < 12) {
                             var dateD = todaysDate.getFullYear() + '-' + (todaysDate.getMonth() + 1) + '-31';
                         }
-                        if (todaysDate.getHours() >= 17) {
+                        if (todaysDate.getHours() >= 12) {
                             if ((0 == todaysDate.getFullYear() % 4) && (0 != todaysDate.getFullYear() % 100) || (0 == todaysDate.getFullYear() % 400)) {
                                 var dateD = todaysDate.getFullYear() + '-' + (todaysDate.getMonth() + 2) + '-29';
                             } else {
@@ -133,10 +133,10 @@ document.addEventListener("DOMContentLoaded", function (event) {
                             var dateD = todaysDate.getFullYear() + '-' + (todaysDate.getMonth() + 1) + '-29';
                         }
                         if (todaysDate.getDate() >= 29) {
-                            if (todaysDate.getHours() < 17) {
+                            if (todaysDate.getHours() < 12) {
                                 var dateD = todaysDate.getFullYear() + '-' + (todaysDate.getMonth() + 1) + '-29';
                             }
-                            if (todaysDate.getHours() >= 17) {
+                            if (todaysDate.getHours() >= 12) {
                                 var dateD = todaysDate.getFullYear() + '-' + (todaysDate.getMonth() + 2) + '-31';
                             }
                         }
@@ -145,10 +145,10 @@ document.addEventListener("DOMContentLoaded", function (event) {
                             var dateD = todaysDate.getFullYear() + '-' + (todaysDate.getMonth() + 1) + '-28';
                         }
                         if (todaysDate.getDate() >= 28) {
-                            if (todaysDate.getHours() < 17) {
+                            if (todaysDate.getHours() < 12) {
                                 var dateD = todaysDate.getFullYear() + '-' + (todaysDate.getMonth() + 1) + '-28';
                             }
-                            if (todaysDate.getHours() >= 17) {
+                            if (todaysDate.getHours() >= 12) {
                                 var dateD = todaysDate.getFullYear() + '-' + (todaysDate.getMonth() + 2) + '-31';
                             }
                         }
@@ -161,10 +161,10 @@ document.addEventListener("DOMContentLoaded", function (event) {
                         var dateD = todaysDate.getFullYear() + '-' + (todaysDate.getMonth() + 1) + '-31';
                     }
                     if (todaysDate.getDate() >= 31) {
-                        if (todaysDate.getHours() < 17) {
+                        if (todaysDate.getHours() < 12) {
                             var dateD = todaysDate.getFullYear() + '-' + (todaysDate.getMonth() + 1) + '-31';
                         }
-                        if (todaysDate.getHours() >= 17) {
+                        if (todaysDate.getHours() >= 12) {
                             var dateD = todaysDate.getFullYear() + '-' + (todaysDate.getMonth() + 2) + '-31';
                         }
                     }
@@ -176,10 +176,10 @@ document.addEventListener("DOMContentLoaded", function (event) {
                         var dateD = todaysDate.getFullYear() + '-' + (todaysDate.getMonth() + 1) + '-31';
                     }
                     if (todaysDate.getDate() >= 31) {
-                        if (todaysDate.getHours() < 17) {
+                        if (todaysDate.getHours() < 12) {
                             var dateD = todaysDate.getFullYear() + '-' + (todaysDate.getMonth() + 1) + '-31';
                         }
-                        if (todaysDate.getHours() >= 17) {
+                        if (todaysDate.getHours() >= 12) {
                             var dateD = (todaysDate.getFullYear() + 1) + '-01-31';
                         }
                     }
@@ -191,10 +191,10 @@ document.addEventListener("DOMContentLoaded", function (event) {
                         var dateD = todaysDate.getFullYear() + '-' + (todaysDate.getMonth() + 1) + '-31';
                     }
                     if (todaysDate.getDate() >= 31) {
-                        if (todaysDate.getHours() < 17) {
+                        if (todaysDate.getHours() < 12) {
                             var dateD = todaysDate.getFullYear() + '-' + (todaysDate.getMonth() + 1) + '-31';
                         }
-                        if (todaysDate.getHours() >= 17) {
+                        if (todaysDate.getHours() >= 12) {
                             var dateD = todaysDate.getFullYear() + '-' + (todaysDate.getMonth() + 2) + '-30';
                         }
                     }
@@ -206,16 +206,16 @@ document.addEventListener("DOMContentLoaded", function (event) {
                         var dateD = todaysDate.getFullYear() + '-' + (todaysDate.getMonth() + 1) + '-30';
                     }
                     if (todaysDate.getDate() >= 30) {
-                        if (todaysDate.getHours() < 17) {
+                        if (todaysDate.getHours() < 12) {
                             var dateD = todaysDate.getFullYear() + '-' + (todaysDate.getMonth() + 1) + '-30';
                         }
-                        if (todaysDate.getHours() >= 17) {
+                        if (todaysDate.getHours() >= 12) {
                             var dateD = todaysDate.getFullYear() + '-' + (todaysDate.getMonth() + 2) + '-31';
                         }
                     }
                 }
 
-                var timeD = "17:00:00";
+                var timeD = "23:59:00";
                 jobdeliverydatetime = dateD + ' ' + timeD;
 
                 var request = new XMLHttpRequest();

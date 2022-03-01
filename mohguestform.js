dde8ec13b2302f39911a563534fc3e547ebfc292
syahmighafar <81664143+syahmighafar@users.noreply.github.com>
2022-03-01 14:14:22 +0800

    Update mohguestform.js

--- mohguestform.js
+++ mohguestform.js
@@ -764,7 +764,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
                 document.getElementById("customerAddress").value = customerAddress;
 
-                var customerPhone = document.getElementById("code").value + document.getElementById("contact_1").value;
+                var customerPhone = "+" + document.getElementById("code").value + document.getElementById("contact_1").value;
                 var additionalPhone = document.getElementById("code_2").value + document.getElementById("contact_2").value;
                 var requesterPhone = document.getElementById("code_3").value + document.getElementById("Requester-Contact-Number").value;
                 var appointmentPlace = document.getElementById("BNHC").value + document.getElementById("TUHC").value

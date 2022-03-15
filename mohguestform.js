5ff17f01d1fe88f433438137a9bf5a344071b7b4
syahmighafar <81664143+syahmighafar@users.noreply.github.com>
2022-03-15 14:09:25 +0800

    Update mohguestform.js

--- mohguestform.js
+++ mohguestform.js
@@ -710,29 +710,20 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
                 //getSubmittedDate
                 var today = new Date();
-                var date = today.getDate() + '-' + (today.getMonth() + 1) + '-' + today.getFullYear();
+                var date = today.getDate() + '/' + (today.getMonth() + 1) + '/' + today.getFullYear();
 
-                var ampm = '';
                 var ampmhour = '';
                 var ampmmin = '';
                 var ampmNum = '';
 
                 if (today.getHours() < 12) {
                     ampmNum = 0;
-                    ampm = 'am';
                     ampmhour = (today.getHours());
                 }
 
-                if (today.getHours() == 12) {
+                if (today.getHours() >= 12) {
                     ampmNum = 1;
-                    ampm = 'pm';
                     ampmhour = (today.getHours());
-                }
-
-                if (today.getHours() > 12) {
-                    ampmNum = 1;
-                    ampm = 'pm';
-                    ampmhour = (today.getHours() - 12);
                 }
 
                 if (today.getMinutes() < 10) {
@@ -743,13 +734,15 @@ document.addEventListener("DOMContentLoaded", function (event) {
                     ampmmin = (today.getMinutes());
                 }
 
-                var time = ampmhour + ":" + ampmmin + " " + ampm;
+                var time = ampmhour + ":" + ampmmin;
 
                 let dateSubmitted = date + ' ' + time;
 
                 document.getElementById("dateSubmitted").value = dateSubmitted;
 
                 let patientOrderId = "MOH" + today.getDate() + (today.getMonth() + 1) + today.getFullYear() + ampmhour + ampmmin + ampmNum;
+
+                var area = "";
 
                 //getCustomerFullAddress
                 if (document.getElementById("Simpang").value.length == 0) {
@@ -761,6 +754,121 @@ document.addEventListener("DOMContentLoaded", function (event) {
                 }
 
                 document.getElementById("customerAddress").value = customerAddress;
+
+                let address = customerAddress.toUpperCase();
+
+                if (address.includes("MANGGIS") == true) { area = "B1"; }
+                else if (address.includes("DELIMA") == true) { area = "B1"; }
+                else if (address.includes("ANGGREK DESA") == true) { area = "B1"; }
+                else if (address.includes("PULAIE") == true) { area = "B1"; }
+                else if (address.includes("LAMBAK") == true) { area = "B1"; }
+                else if (address.includes("TERUNJING") == true) { area = "B1"; }
+                else if (address.includes("MADANG") == true) { area = "B1"; }
+                else if (address.includes("AIRPORT") == true) { area = "B1"; }
+                else if (address.includes("ORANG KAYA BESAR IMAS") == true) { area = "B1"; }
+                else if (address.includes("OKBI") == true) { area = "B1"; }
+                else if (address.includes("SERUSOP") == true) { area = "B1"; }
+                else if (address.includes("BURONG PINGAI") == true) { area = "B1"; }
+                else if (address.includes("SETIA NEGARA") == true) { area = "B1"; }
+                else if (address.includes("PASIR BERAKAS") == true) { area = "B1"; }
+                else if (address.includes("MENTERI BESAR") == true) { area = "B1"; }
+                else if (address.includes("KEBANGSAAN LAMA") == true) { area = "B1"; }
+                else if (address.includes("BATU MARANG") == true) { area = "B2"; }
+                else if (address.includes("DATO GANDI") == true) { area = "B2"; }
+                else if (address.includes("KAPOK") == true) { area = "B2"; }
+                else if (address.includes("KOTA BATU") == true) { area = "B2"; }
+                else if (address.includes("MENTIRI") == true) { area = "B2"; }
+                else if (address.includes("MERAGANG") == true) { area = "B2"; }
+                else if (address.includes("PELAMBAIAN") == true) { area = "B2"; }
+                else if (address.includes("PINTU MALIM") == true) { area = "B2"; }
+                else if (address.includes("SALAMBIGAR") == true) { area = "B2"; }
+                else if (address.includes("SALAR") == true) { area = "B2"; }
+                else if (address.includes("SERASA") == true) { area = "B2"; }
+                else if (address.includes("MUARA") == true) { area = "B2"; }
+                else if (address.includes("SERDANG") == true) { area = "B2"; }
+                else if (address.includes("SUNGAI BASAR") == true) { area = "B2"; }
+                else if (address.includes("SG BASAR") == true) { area = "B2"; }
+                else if (address.includes("SUNGAI BELUKUT") == true) { area = "B2"; }
+                else if (address.includes("SG BELUKUT") == true) { area = "B2"; }
+                else if (address.includes("SUNGAI HANCHING") == true) { area = "B2"; }
+                else if (address.includes("SG HANCHING") == true) { area = "B2"; }
+                else if (address.includes("SUNGAI TILONG") == true) { area = "B2"; }
+                else if (address.includes("SG TILONG") == true) { area = "B2"; }
+                else if (address.includes("SUBOK") == true) { area = "B2"; }
+                else if (address.includes("SUNGAI AKAR") == true) { area = "B2"; }
+                else if (address.includes("SG AKAR") == true) { area = "B2"; }
+                else if (address.includes("SUNGAI BULOH") == true) { area = "B2"; }
+                else if (address.includes("SG BULOH") == true) { area = "B2"; }
+                else if (address.includes("TANAH JAMBU") == true) { area = "B2"; }
+                else if (address.includes("SUNGAI OROK") == true) { area = "B2"; }
+                else if (address.includes("SG OROK") == true) { area = "B2"; }
+                else if (address.includes("KATOK") == true) { area = "G1"; }
+                else if (address.includes("MATA-MATA") == true) { area = "G1"; }
+                else if (address.includes("RIMBA") == true) { area = "G1"; }
+                else if (address.includes("TUNGKU") == true) { area = "G1"; }
+                else if (address.includes("UBD") == true) { area = "G1"; }
+                else if (address.includes("JIS") == true) { area = "G1"; }
+                else if (address.includes("BERANGAN") == true) { area = "G2"; }
+                else if (address.includes("BERIBI") == true) { area = "G2"; }
+                else if (address.includes("KIULAP") == true) { area = "G2"; }
+                else if (address.includes("RIPAS") == true) { area = "G2"; }
+                else if (address.includes("RAJA ISTERI PENGIRAN ANAK SALLEHA") == true) { area = "G2"; }
+                else if (address.includes("KIARONG") == true) { area = "G2"; }
+                else if (address.includes("PUSAR ULAK") == true) { area = "G2"; }
+                else if (address.includes("KUMBANG PASANG") == true) { area = "G2"; }
+                else if (address.includes("MENGLAIT") == true) { area = "G2"; }
+                else if (address.includes("MABOHAI") == true) { area = "G2"; }
+                else if (address.includes("ONG SUM PING") == true) { area = "G2"; }
+                else if (address.includes("GADONG") == true) { area = "G2"; }
+                else if (address.includes("TASEK LAMA") == true) { area = "G2"; }
+                else if (address.includes("BANDAR TOWN") == true) { area = "G2"; }
+                else if (address.includes("BATU SATU") == true) { area = "JT1"; }
+                else if (address.includes("BENGKURONG") == true) { area = "JT1"; }
+                else if (address.includes("BUNUT") == true) { area = "JT1"; }
+                else if (address.includes("JALAN BABU RAJA") == true) { area = "JT1"; }
+                else if (address.includes("JALAN ISTANA") == true) { area = "JT1"; }
+                else if (address.includes("JUNJONGAN") == true) { area = "JT1"; }
+                else if (address.includes("KASAT") == true) { area = "JT1"; }
+                else if (address.includes("LUMAPAS") == true) { area = "JT1"; }
+                else if (address.includes("JALAN HALUS") == true) { area = "JT1"; }
+                else if (address.includes("MADEWA") == true) { area = "JT1"; }
+                else if (address.includes("PUTAT") == true) { area = "JT1"; }
+                else if (address.includes("SINARUBAI") == true) { area = "JT1"; }
+                else if (address.includes("TASEK MERADUN") == true) { area = "JT1"; }
+                else if (address.includes("TELANAI") == true) { area = "JT1"; }
+                else if (address.includes("BAN") == true) { area = "JT2"; }
+                else if (address.includes("BATONG") == true) { area = "JT2"; }
+                else if (address.includes("BATU AMPAR") == true) { area = "JT2"; }
+                else if (address.includes("BEBATIK") == true) { area = "JT2"; }
+                else if (address.includes("BEBULOH") == true) { area = "JT2"; }
+                else if (address.includes("BEBATIK KILANAS") == true) { area = "JT2"; }
+                else if (address.includes("KILANAS") == true) { area = "JT2"; }
+                else if (address.includes("DADAP") == true) { area = "JT2"; }
+                else if (address.includes("KUALA LURAH") == true) { area = "JT2"; }
+                else if (address.includes("KULAPIS") == true) { area = "JT2"; }
+                else if (address.includes("LIMAU MANIS") == true) { area = "JT2"; }
+                else if (address.includes("MASIN") == true) { area = "JT2"; }
+                else if (address.includes("MULAUT") == true) { area = "JT2" }
+                else if (address.includes("PANCHOR MURAI") == true) { area = "JT2"; }
+                else if (address.includes("PANCHUR MURAI") == true) { area = "JT2"; }
+                else if (address.includes("PANGKALAN BATU") == true) { area = "JT2"; }
+                else if (address.includes("PASAI") == true) { area = "JT2"; }
+                else if (address.includes("WASAN") == true) { area = "JT2"; }
+                else if (address.includes("PARIT") == true) { area = "JT2"; }
+                else if (address.includes("EMPIRE") == true) { area = "JT3"; }
+                else if (address.includes("JANGSAK") == true) { area = "JT3"; }
+                else if (address.includes("JERUDONG") == true) { area = "JT3"; }
+                else if (address.includes("KATIMAHAR") == true) { area = "JT3"; }
+                else if (address.includes("LUGU") == true) { area = "JT3"; }
+                else if (address.includes("SENGKURONG") == true) { area = "JT3"; }
+                else if (address.includes("TANJONG NANGKA") == true) { area = "JT3"; }
+                else if (address.includes("TANJONG BUNUT") == true) { area = "JT3"; }
+                else if (address.includes("TANJUNG BUNUT") == true) { area = "JT3"; }
+                else if (address.includes("SUNGAI TAMPOI") == true) { area = "JT3"; }
+                else if (address.includes("SG TAMPOI") == true) { area = "JT3"; }
+                else { area = "N/A"; }
+
+                document.getElementById("area").value = area;
 
                 if (document.querySelector('input[name=District]:checked').value == "Brunei Muara") {
                     if ((document.getElementById("BNHC").value == "Raja Isteri Pengiran Anak Saleha Hospital") ||
@@ -981,7 +1089,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
                                 fetch(scriptURL, { method: 'POST', body: new FormData(form) })
                                     .catch(error => console.error('Error!', error.message))
-                                
+
                                 if (createPharmacySheet == 1) {
                                     if (todChoice == "Standard") {
                                         const scriptURL = 'https://script.google.com/macros/s/AKfycbxxRcpKi-hcJiyKBvvB12ZYbEeZ6YS2XE9byGVO3031_91nXja45Qa62jieHfZ3cLPOdw/exec'

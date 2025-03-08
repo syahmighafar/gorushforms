591b335ec03344330a35de7e6ebbdcbcc54b1836
syahmighafar <81664143+syahmighafar@users.noreply.github.com>
2025-03-08 08:14:35 +0800

    Update atestguestform.js

--- atestguestform.js
+++ atestguestform.js
@@ -1998,7 +1998,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
                                                 (year !== 2025 || month !== 2 || (date !== 2 && date !== 3))
                                             ) {
                                                 if ((day != 0) && (day != 5)) {
-                                                    if ((hour >= 8) && (hour < 15)) {
+                                                    if ((hour >= 8) && (hour < 11)) {
                                                         $("#mohimmidiate").fadeIn();
                                                     } else {
                                                         $("#mohimmidiate").hide();
@@ -2129,7 +2129,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
                                                 (year !== 2025 || month !== 2 || (date !== 2 && date !== 3))
                                             ) {
                                                 if ((day != 0) && (day != 5)) {
-                                                    if ((hour >= 8) && (hour < 15)) {
+                                                    if ((hour >= 8) && (hour < 11)) {
                                                         $("#mohimmidiate").fadeIn();
                                                     } else {
                                                         $("#mohimmidiate").hide();

e646f401361ee523c72724db1c5a0563f7e8d85e
syahmighafar <81664143+syahmighafar@users.noreply.github.com>
2025-06-14 11:43:00 +0800

    Update careermemberform.js

--- careermemberform.js
+++ careermemberform.js
@@ -44,58 +44,198 @@ document.addEventListener("DOMContentLoaded", function (event) {
         document.getElementById("personalNextBtn").addEventListener("click", function () {
                 $("#personalNextBtn").hide();
                 $("#personalsection").hide();
-                $("#applicationNextBtn").fadeIn();
+                $("#applicationNextBtn").hide();
                 $("#applicationPrevBtn").fadeIn();
                 $("#applicationsection").fadeIn();
 
                 window.scrollTo({ top: 0, behavior: 'smooth' });
         });
 
+        $('#positionapplied').change(function () {
+            if (($(this).val().length == 0)) {
+                $("#partTimeDurationArea").hide();
+                $("#freelancerArea").hide();
+                $("#experienceDeliveryArea").hide();
+                document.getElementById("partTimeDuration").value = "";
+                document.getElementById("carOwn").value = "";
+                document.getElementById("deliverBefore").value = "";
+                document.getElementById("experienceDelivery").value = "";
+                document.getElementById("parcelNum").value = "";
+
+                $("#applicationNextBtn").hide();
+            }
+
+            if (($(this).val() == "Freelancer")) {
+                $("#partTimeDurationArea").fadeIn();
+                $("#freelancerArea").fadeIn();
+                $("#experienceDeliveryArea").hide();
+                document.getElementById("partTimeDuration").value = "";
+                document.getElementById("carOwn").value = "";
+                document.getElementById("deliverBefore").value = "";
+                document.getElementById("experienceDelivery").value = "";
+                document.getElementById("parcelNum").value = "";
+
+                $("#applicationNextBtn").fadeIn();
+            }
+        })
+
+        $('#deliverBefore').change(function () {
+            if (($(this).val().length == 0)) {
+                $("#experienceDeliveryArea").hide();
+                document.getElementById("experienceDelivery").value = "";
+                document.getElementById("parcelNum").value = "";
+            }
+
+            if (($(this).val() == "Yes")) {
+                $("#experienceDeliveryArea").fadeIn();
+                document.getElementById("experienceDelivery").value = "";
+                document.getElementById("parcelNum").value = "";
+            }
+
+            if (($(this).val() == "No")) {
+                $("#experienceDeliveryArea").hide();
+                document.getElementById("experienceDelivery").value = "";
+                document.getElementById("parcelNum").value = "";
+            }
+        })
+
         document.getElementById("applicationNextBtn").addEventListener("click", function () {
             if (($("#positionapplied").val().length == 0)) {
                 $("#agreementsection").hide();
-                alert("Please do not leave the position empty!");
+                alert("Please choose your position!");
             }
 
             if (($("#highestachievement").val().length == 0)) {
                 $("#agreementsection").hide();
-                alert("Please do not leave the highest achievement field empty!");
+                alert("Please choose your highest achievement!");
             }
 
             if (($("#positionapplied").val().length != 0) && ($("#highestachievement").val().length != 0)) {
+if ($("#positionapplied").val() == "Freelancer") {
+                    if (($("#partTimeDuration").val().length == 0)) {
+                        alert("Please choose on how long do you intend to stay in the part time role!");
+                    }
 
-                document.getElementById("namesum").innerHTML = "Name: <b>" + document.getElementById("name").value + "</b>";
-                document.getElementById("dateofbirthsum").innerHTML = "Date of Birth: <b>" + document.getElementById("dateofbirth").value + "</b>";
-                document.getElementById("icnumbersum").innerHTML = "IC Number: <b>" + document.getElementById("icnumber").value + "</b>";
-                document.getElementById("addresssum").innerHTML = "Address: <b>" + document.getElementById("house").value + " " + document.getElementById("kampong").value
-                    + " " + document.getElementById("jalan").value + " " + document.getElementById("simpang").value + " " + document.getElementById("district").value + " "
-                    + document.getElementById("postal").value + "</b>";
+                    if (($("#carOwn").val().length == 0)) {
+                        alert("Please choose what type of car that you own!");
+                    }
 
-                document.getElementById("emailsum").innerHTML = "Email: <b>" + document.getElementById("email").value + "</b>";
+                    if (($("#deliverBefore").val().length == 0)) {
+                        alert("Please choose if you had done delivery work before!");
+                    }
 
-                document.getElementById("contact_1sum").innerHTML = "Phone Number: <b>+" + document.getElementById("code").value + document.getElementById("contact_1").value.replace(" ","") + "</b>";
+                    if (($("#deliverBefore").val() == "Yes")) {
+                        if (($("#experienceDelivery").val().length == 0)) {
+                            alert("Please choose how long did you do delivery work!");
+                        }
 
-                if ($("#contact_2").val().length == 0) {
-                    document.getElementById("contact_2sum").innerHTML = "Additional Phone Number: <b>N/A</b>";
+                        if (($("#parcelNum").val().length == 0)) {
+                            alert("Please choose how many parcels can you delivery in a day!");
+                        }
+
+                        if (($("#experienceDelivery").val().length != 0) && ($("#parcelNum").val().length != 0)) {
+                            document.getElementById("namesum").innerHTML = "Name: <b>" + document.getElementById("name").value + "</b>";
+                            document.getElementById("dateofbirthsum").innerHTML = "Date of Birth: <b>" + document.getElementById("dateofbirth").value + "</b>";
+                            document.getElementById("icnumbersum").innerHTML = "IC Number: <b>" + document.getElementById("icnumber").value + "</b>";
+                            document.getElementById("addresssum").innerHTML = "Address: <b>" + document.getElementById("house").value + " " + document.getElementById("kampong").value
+                                + " " + document.getElementById("jalan").value + " " + document.getElementById("simpang").value + " " + document.getElementById("district").value + " "
+                                + document.getElementById("postal").value + "</b>";
+
+                            document.getElementById("emailsum").innerHTML = "Email: <b>" + document.getElementById("email").value + "</b>";
+
+                            document.getElementById("contact_1sum").innerHTML = "Phone Number: <b>+" + document.getElementById("code").value + document.getElementById("contact_1").value + "</b>";
+
+                            if ($("#contact_2").val().length == 0) {
+                                document.getElementById("contact_2sum").innerHTML = "Additional Phone Number: <b>N/A</b>";
+                            }
+
+                            if ($("#contact_2").val().length != 0) {
+                                document.getElementById("contact_2sum").innerHTML = "Additional Phone Number: <b>+" + document.getElementById("code_2").value + document.getElementById("contact_2").value + "</b>";
+                            }
+
+                            document.getElementById("positionappliedsum").innerHTML = "Position Applied: <b>" + document.getElementById("positionapplied").value + "</b>";
+
+                            document.getElementById("highestachievementsum").innerHTML = "Highest Qualification Achieved: <b>" + document.getElementById("highestachievement").value + "</b>";
+
+                            document.getElementById("partTimeDurationsum").innerHTML = "Duration of Part-Time work expected: <b>" + document.getElementById("partTimeDuration").value + "</b>";
+
+                            document.getElementById("carOwnsum").innerHTML = "Type of transportation own: <b>" + document.getElementById("carOwn").value + "</b>";
+
+                            document.getElementById("deliverBeforesum").innerHTML = "Done delivery work before?: <b>" + document.getElementById("deliverBefore").value + "</b>";
+
+                            document.getElementById("experienceDeliverysum").innerHTML = "Duration of past delivery work: <b>" + document.getElementById("experienceDelivery").value + "</b>";
+
+                            document.getElementById("parcelNumsum").innerHTML = "Estimated parcels delivered in a day: <b>" + document.getElementById("parcelNum").value + "</b>";
+
+                            $("#partTimeDurationsum").fadeIn();
+                            $("#carOwnsum").fadeIn();
+                            $("#deliverBeforesum").fadeIn();
+                            $("#experienceDeliverysum").fadeIn();
+                            $("#parcelNumsum").fadeIn();
+
+                            $("#applicationNextBtn").hide();
+                            $("#applicationPrevBtn").hide();
+                            $("#applicationsection").hide();
+                            $("#submitBtn").fadeIn();
+                            $("#agreesubmitPrevBtn").fadeIn();
+                            $("#agreementsection").fadeIn();
+                            $("#ensure").hide();
+
+                            window.scrollTo({ top: 0, behavior: 'smooth' });
+                        }
+                    }
                 }
 
-                if ($("#contact_2").val().length != 0) {
-                    document.getElementById("contact_2sum").innerHTML = "Additional Phone Number: <b>+" + document.getElementById("code_2").value + document.getElementById("contact_2").value.replace(" ","") + "</b>";
+                if (($("#deliverBefore").val() == "No")) {
+                    document.getElementById("namesum").innerHTML = "Name: <b>" + document.getElementById("name").value + "</b>";
+                    document.getElementById("dateofbirthsum").innerHTML = "Date of Birth: <b>" + document.getElementById("dateofbirth").value + "</b>";
+                    document.getElementById("icnumbersum").innerHTML = "IC Number: <b>" + document.getElementById("icnumber").value + "</b>";
+                    document.getElementById("addresssum").innerHTML = "Address: <b>" + document.getElementById("house").value + " " + document.getElementById("kampong").value
+                        + " " + document.getElementById("jalan").value + " " + document.getElementById("simpang").value + " " + document.getElementById("district").value + " "
+                        + document.getElementById("postal").value + "</b>";
+
+                    document.getElementById("emailsum").innerHTML = "Email: <b>" + document.getElementById("email").value + "</b>";
+
+                    document.getElementById("contact_1sum").innerHTML = "Phone Number: <b>+" + document.getElementById("code").value + document.getElementById("contact_1").value + "</b>";
+
+                    if ($("#contact_2").val().length == 0) {
+                        document.getElementById("contact_2sum").innerHTML = "Additional Phone Number: <b>N/A</b>";
+                    }
+
+                    if ($("#contact_2").val().length != 0) {
+                        document.getElementById("contact_2sum").innerHTML = "Additional Phone Number: <b>+" + document.getElementById("code_2").value + document.getElementById("contact_2").value + "</b>";
+                    }
+
+                    document.getElementById("positionappliedsum").innerHTML = "Position Applied: <b>" + document.getElementById("positionapplied").value + "</b>";
+
+                    document.getElementById("highestachievementsum").innerHTML = "Highest Qualification Achieved: <b>" + document.getElementById("highestachievement").value + "</b>";
+
+                    document.getElementById("partTimeDurationsum").innerHTML = "Duration of Part-Time work expected: <b>" + document.getElementById("partTimeDuration").value + "</b>";
+
+                    document.getElementById("carOwnsum").innerHTML = "Type of transportation own: <b>" + document.getElementById("carOwn").value + "</b>";
+
+                    document.getElementById("deliverBeforesum").innerHTML = "Done delivery work before?: <b>" + document.getElementById("deliverBefore").value + "</b>";
+
+                    document.getElementById("experienceDeliverysum").innerHTML = "";
+
+                    document.getElementById("parcelNumsum").innerHTML = "";
+
+                    $("#partTimeDurationsum").fadeIn();
+                    $("#carOwnsum").fadeIn();
+                    $("#deliverBeforesum").fadeIn();
+                    $("#experienceDeliverysum").hide();
+                    $("#parcelNumsum").hide();
+
+                    $("#applicationNextBtn").hide();
+                    $("#applicationPrevBtn").hide();
+                    $("#applicationsection").hide();
+                    $("#submitBtn").fadeIn();
+                    $("#agreesubmitPrevBtn").fadeIn();
+                    $("#agreementsection").fadeIn();
+                    $("#ensure").hide();
+
+                    window.scrollTo({ top: 0, behavior: 'smooth' });
                 }
-
-                document.getElementById("positionappliedsum").innerHTML = "Position Applied: <b>" + document.getElementById("positionapplied").value + "</b>";
-
-                document.getElementById("highestachievementsum").innerHTML = "Highest Qualification Achieved: <b>" + document.getElementById("highestachievement").value + "</b>";
-
-                $("#applicationNextBtn").hide();
-                $("#applicationPrevBtn").hide();
-                $("#applicationsection").hide();
-                $("#submitBtn").fadeIn();
-                $("#agreesubmitPrevBtn").fadeIn();
-                $("#agreementsection").fadeIn();
-                $("#ensure").hide();
-
-                window.scrollTo({ top: 0, behavior: 'smooth' });
             }
         });
 
